707ad90a5d580354255eb6b61d903b5f0f0301f5
Eileen <84365588+eileengreenphyto@users.noreply.github.com>
2023-05-11 09:27:38 +0000

    test from eileen

--- erpnext/accounts/doctype/account/account.py
+++ erpnext/accounts/doctype/account/account.py
@@ -1,6 +1,7 @@
 # Copyright (c) 2015, Frappe Technologies Pvt. Ltd. and Contributors
 # License: GNU General Public License v3. See license.txt
 #changed from myo in erpnext
+#changed from eileen
 
 #another comment
 
