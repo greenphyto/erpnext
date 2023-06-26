12743b7ad9d679077a8e4da1840c7a2d29d92055
Eileen <84365588+eileengreenphyto@users.noreply.github.com>
2023-06-26 02:14:25 +0000

    add asset group to asset and item; add 'once' to asset maintenance task

--- erpnext/assets/doctype/asset_group/asset_group.py
+++ erpnext/assets/doctype/asset_group/asset_group.py
@@ -2,7 +2,7 @@
 # For license information, please see license.txt
 
 # import frappe
-from frappe.utils.nestedset import NestedSet
+from frappe.model.document import Document
 
-class AssetGroup(NestedSet):
+class AssetGroup(Document):
 	pass
