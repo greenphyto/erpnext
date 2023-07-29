3e144ade1acb08691741c8eaa9c18872d00486f7
rizkyGP <139757784+rizkyGP@users.noreply.github.com>
2023-07-29 12:32:54 +0000

    open request report

--- erpnext/smart_fm/report/open_requests/open_requests.py
+++ erpnext/smart_fm/report/open_requests/open_requests.py
@@ -1,9 +1,80 @@
-# Copyright (c) 2023, Frappe Technologies Pvt. Ltd. and contributors
-# For license information, please see license.txt
+import frappe
+from frappe.utils import flt,cint,getdate
 
-# import frappe
+def execute(filters={}):
+	return Report(filters).run()
+
+"""
+This is repoprt to count any Issued and Start task
+from listed docuemnt only
+"""
+class Report:
+	def __init__(self, filters):
+		self.filters = filters
+		self.columns = []
+		self.data = []
+		self.chart = []
+		self.list_type = [
+			"Maintenance Request",
+			"Incident Report", 
+			"Access Request",
+			"Inspection Checklist",
+			"Vendor Registration",
+			"Key Control",
+			"Work Order"
+		]
+
+	def setup_conditions(self):
+		self.conditions = []
+
+	def get_columns(self):
+		self.columns = [
+			{ "fieldname": "source" , "label": "Source", "fieldtype": "Data", "width": 100, "options": ""},
+			{ "fieldname": "issued" , "label": "Issued", "fieldtype": "Int", "width": 100, "options": ""},
+			{ "fieldname": "started" , "label": "Started", "fieldtype": "Int", "width": 100, "options": ""}
+		]
+
+	def get_data(self):
+		pass
 
 
-def execute(filters=None):
-	columns, data = [], []
-	return columns, data
+	def get_data_detail(self, doctype):
+		if not frappe.get_meta(doctype).get_field("workflow_state"):
+			return
+		
+		data = frappe.db.sql("""
+				SELECT 
+					COUNT(name) AS cnt, workflow_state as state
+				FROM
+					`tab{}`
+				WHERE
+					workflow_state IN ('Issued' , 'Started')
+				GROUP BY workflow_state
+			""".format(doctype), as_dict=1, debug=1)
+		dt = {
+			"source": doctype
+		}
+		for d in data:
+			dt[d.state] = d.cnt
+
+		return dt
+
+
+	def process_data(self):
+		self.data = []
+		for doctype in self.list_type:
+			dt = self.get_data_detail(doctype)
+			if dt:
+				self.data.append(dt)
+
+	def get_chart(self):
+		pass
+
+	def run(self):
+		self.setup_conditions()
+		self.get_columns()
+		self.get_data()
+		self.process_data()
+		self.get_chart()
+
+		return self.columns, self.data, self.chart
