85d5f921f7c7e5bb515d80de4362688d31150bc8
rizkyGP <139757784+rizkyGP@users.noreply.github.com>
2023-08-02 08:57:46 +0000

    format to tree view

--- erpnext/smart_fm/doctype/cleaning_and_security_checklist/cleaning_and_security_checklist.js
+++ erpnext/smart_fm/doctype/cleaning_and_security_checklist/cleaning_and_security_checklist.js
@@ -15,6 +15,8 @@ frappe.ui.form.on('Cleaning and Security Checklist', {
 				}
 			}
 		})
+
+		frm.cscript.refresh_tree_view()
 	},
 	template: function(frm){
 		frm.cscript.load_template()
@@ -34,5 +36,30 @@ $.extend(cur_frm.cscript, {
 				cur_frm.refresh();
 			}
 		})
+	},
+	refresh_tree_view: function(){
+		var frm = this.frm;
+		$.each(frm.doc.checklist, (i, row)=>{
+			frm.cscript.change_indent_view(row.doctype, row.name);
+		})
+	},
+	change_indent_view: function(cdt,cdn){
+		var row = locals[cdt][cdn];
+		if (!row) return;
+		var selector = `.grid-row[data-name="${row.name}"] .grid-static-col[data-fieldname="indicator"]`;
+		if (! $(selector).length ){
+			return;
+		}
+		var el = $(selector)[0];
+		var padding = 20;
+		var pad_left = padding * cint(row.indent);
+		if (pad_left){
+			el.style.setProperty("padding-left", `${pad_left}px`, "important");
+		}
+		if (cint(row.is_group)){
+			el.style.setProperty("font-weight", `bold`, "important");
+		}else{
+			el.style.setProperty("font-weight", ``);
+		}
 	}
 })
