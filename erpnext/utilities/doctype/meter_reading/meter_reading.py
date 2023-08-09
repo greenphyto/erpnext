5472c56465248b30d2e177702d0b0892b800f5a0
rizkyGP <139757784+rizkyGP@users.noreply.github.com>
2023-08-09 07:35:29 +0000

    adjust meter reading

--- erpnext/utilities/doctype/meter_reading/meter_reading.py
+++ erpnext/utilities/doctype/meter_reading/meter_reading.py
@@ -1,8 +1,26 @@
 # Copyright (c) 2023, Frappe Technologies Pvt. Ltd. and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class MeterReading(Document):
-	pass
+	def validate(self):
+		self.set_status()
+
+	def on_submit(self):
+		self.set_status(True)
+
+	def on_cancel(self):
+		self.set_status(True)
+
+	def set_status(self, update_db=False):
+		if self.docstatus == 0:
+			self.status = "Draft"
+		elif self.docstatus == 1:
+			self.status = "Submitted"
+		else:
+			self.status = "Cancelled"
+
+		if update_db:
+			self.db_update()
