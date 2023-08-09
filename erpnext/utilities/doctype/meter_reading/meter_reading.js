8912cc796c12f6afe244b85e5a2c4e6f27c5a0d3
rizkyGP <139757784+rizkyGP@users.noreply.github.com>
2023-08-09 07:25:25 +0000

    label meter reading

--- erpnext/utilities/doctype/meter_reading/meter_reading.js
+++ erpnext/utilities/doctype/meter_reading/meter_reading.js
@@ -2,7 +2,13 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Meter Reading', {
-	// refresh: function(frm) {
-
-	// }
+	type_of_meter: function(frm) {
+		if (frm.doc.type_of_meter=="Electricity"){
+			frm.set_currency_labels(['current_reading'], "kWh");
+		}else if (frm.doc.type_of_meter=="Water"){
+			frm.set_currency_labels(['current_reading'], "m3");
+		}else if (frm.doc.type_of_meter=="Gas"){
+			frm.set_currency_labels(['current_reading'], "kWh");
+		}
+	}
 });
