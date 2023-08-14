09d2ce2d62d3e0df200f6e46b7b8ccd216606dbc
rizkyGP <139757784+rizkyGP@users.noreply.github.com>
2023-08-14 06:17:00 +0000

    get meter reading per month

--- erpnext/smart_fm/report/meter_reading_report/meter_reading_report.py
+++ erpnext/smart_fm/report/meter_reading_report/meter_reading_report.py
@@ -1,5 +1,9 @@
 import frappe
 from frappe.utils import flt,cint,getdate
+from erpnext.smart_fm.report.utility_report.utility_report import get_last_meter_data
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+from dateutil import rrule
 
 def execute(filters={}):
 	return Report(filters).run()
@@ -36,23 +40,71 @@ class Report:
 
 	def setup_conditions(self):
 		self.conditions = []
-
+		if self.filters.group_by == "Utility":
+			self.key_id = "meter_id"
+		else:
+			self.key_id = "type_of_meter"
 
 	def get_columns(self):
 		self.columns = [
-			{ "fieldname": "" , "label": "", "fieldtype": "Data", "width": 100, "options": ""}
+			{ "fieldname": "meter_id" ,         "label": "Meter ID",        "fieldtype": "Link",    "width": 120, "options": "Utility"},
+            { "fieldname": "type_of_meter" ,    "label": "Type of Meter",   "fieldtype": "Data",    "width": 150, "options": ""},
+            { "fieldname": "meter_location" ,   "label": "Meter Location",  "fieldtype": "Data",    "width": 150, "options": ""}
 		]
+
+		for d in self.column_date:
+			print(d)
+			self.columns.append({
+				"label": self.get_column_label(d),
+				"fieldname": self.get_column_key(d),
+				"fieldtype": "Float",
+				"precision": 2,
+				"width": 120
+			})
+
+	def get_column_key(self, date):
+		return frappe.scrub(self.get_column_label(date))
+
+	def get_column_label (self, date):
+		return date.strftime("%b %y")
 		
 	def get_periode(self):
-		pass
+		# for d in 
+		start_date = getdate(self.filters.from_date).replace(day=1)
+		end_date = getdate(self.filters.to_date).replace(day=1)
+		self.periode = {}
+		self.column_date = []
+		for dt in rrule.rrule(rrule.MONTHLY, dtstart=start_date, until=end_date):
+			name = self.get_column_label(dt)
+			nd = dt + relativedelta(months=1) - relativedelta(days=1)
+			self.periode[name] = [dt, nd]
+			self.column_date.append(dt)
+			print(dt, nd)
+
+		print(start_date, end_date)
 
 	def get_data(self):
-		self.raw_data = frappe.db.sql("""
-			select * from tabItem limit 10
-		""")
+		self.raw_data = []
+		self.data_dict = {}
+		for key, dates in self.periode.items():
+			last_date = dates[1]
+			data = get_last_meter_data({"reading_date": last_date})
+			for d in data:
+				key_name = d.get(self.key_id)
+				if key_name not in self.data_dict:
+					self.data_dict[key_name] = d
+					row = d
+				else:
+					row = self.data_dict[key_name]
+				
+				row[self.get_column_key(dates[0])] = d.current_reading
+		
+		print(self.data_dict)
 
 	def process_data(self):
-		self.data = self.raw_data
+		self.data = []
+		for key, d in self.data_dict.items():
+			self.data.append(d)
 
 	def get_chart(self):
 		pass
--- erpnext/smart_fm/report/utility_report/utility_report.py
+++ erpnext/smart_fm/report/utility_report/utility_report.py
@@ -12,14 +12,7 @@ class Report:
         self.chart = []
 
     def setup_conditions(self):
-        self.conditions = ""
-        if self.filters.get("type_of_meter"):
-            self.conditions += " and u.type_of_meter = %(type_of_meter)s "
-        elif self.filters.get("utility"):
-            self.conditions += " and u.name = %(utility)s "
-
-        if self.filters.get("reading_date"):
-            self.conditions += " and m.reading_date <= %(reading_date)s "
+        pass
 
 
     def get_columns(self):
@@ -32,27 +25,7 @@ class Report:
         ]
 
     def get_data(self):
-        self.raw_data = frappe.db.sql("""
-            SELECT 
-                *
-            FROM
-                (SELECT DISTINCT
-                        m.meter_id,
-                        m.current_reading,
-                        m.reading_date,
-                        m.reader_nameid,
-                        u.type_of_meter,
-                        u.meter_location
-                FROM
-                    `tabMeter Reading` m
-                LEFT JOIN `tabUtility` u ON u.name = m.meter_id
-                WHERE 
-                    m.docstatus = 1
-                    {}
-                ORDER BY u.reading_date DESC
-            ) AS m1
-            GROUP BY m1.meter_id
-        """.format(self.conditions), self.filters, as_dict=1)
+        self.raw_data = get_last_meter_data(self.filters);
 
     def process_data(self):
         self.data = self.raw_data
@@ -96,3 +69,37 @@ class Report:
         self.get_chart()
 
         return self.columns, self.data, None, self.chart
+
+def get_last_meter_data(filters):
+    conditions = ""
+    if filters.get("type_of_meter"):
+        conditions += " and u.type_of_meter = %(type_of_meter)s "
+    elif filters.get("utility"):
+        conditions += " and u.name = %(utility)s "
+    if filters.get("reading_date"):
+        conditions += " and m.reading_date <= %(reading_date)s "
+
+    data = frappe.db.sql("""
+            SELECT 
+                *
+            FROM
+                (SELECT DISTINCT
+                        m.meter_id,
+                        m.current_reading,
+                        m.reading_date,
+                        m.reader_nameid,
+                        u.type_of_meter,
+                        u.meter_location
+                FROM
+                    `tabMeter Reading` m
+                LEFT JOIN `tabUtility` u ON u.name = m.meter_id
+                WHERE 
+                    m.docstatus = 1
+                    {}
+                ORDER BY u.reading_date DESC
+            ) AS m1
+            GROUP BY m1.meter_id    
+                         
+        """.format(conditions), filters, as_dict=1)
+
+    return data
