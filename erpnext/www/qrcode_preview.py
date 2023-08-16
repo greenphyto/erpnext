7c95443e67919c445d59ac65ee0fea29f99910ff
rizkyGP <139757784+rizkyGP@users.noreply.github.com>
2023-08-16 04:09:52 +0000

    create qrcode online page

---
+++ erpnext/www/qrcode_preview.py
@@ -0,0 +1,13 @@
+import frappe
+
+def get_context(context):
+    args = frappe.form_dict
+    doctype = args.get("doctype")
+    name = args.get("name")
+    if not doctype or not name:
+        return
+    
+    if not frappe.db.exists(doctype, name):
+        return
+    
+    context.doc = frappe.get_doc("Asset", "ACC-ASS-2023-00001")
