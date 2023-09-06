21a94020d4c5f1f17bb79e4f1d01125da8046d26
rizkyGP <139757784+rizkyGP@users.noreply.github.com>
2023-09-06 07:02:18 +0000

    update existing supplier

--- erpnext/patches/v14_0/update_customer_name.py
+++ erpnext/patches/v14_0/update_customer_name.py
@@ -13,5 +13,25 @@ def delete_existing_property_setters():
     frappe.delete_doc("Property Setter", "Customer-naming_series-options", ignore_missing=True)
     frappe.delete_doc("Property Setter", "Supplier-naming_series-options", ignore_missing=True)
 
+from frappe import rename_doc
 def update_existing_customer():
-    pass
+    _update_existing_data("Customer")
+
+def update_existing_supplier():
+    _update_existing_data("Supplier")
+
+def _update_existing_data(doctype):
+    data = frappe.db.get_list(doctype, {"name":['not like', '%%C00%%']})
+    print("Total {} {} to be rename".format(len(data), doctype))
+    for d in data:
+        doc = frappe.get_doc(doctype, d.name)
+        old_name = str(doc.name)
+        if doctype == "Customer":
+            doc.naming_series = "C.#####"
+        else:
+            doc.naming_series = "S.#####"
+
+        doc.autoname()
+        new_name = doc.name
+        print("Rename {} {} to {}".format(doctype, d.name, new_name))
+        rename_doc(doctype, old_name, new_name)
