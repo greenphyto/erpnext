65745f97478e6ccf0f8ec5408a774ce8850b6f9b
rizkyGP <139757784+rizkyGP@users.noreply.github.com>
2023-10-31 07:46:36 +0000

    set default value building feedback

--- erpnext/smart_fm/doctype/building_environment_feedback/building_environment_feedback.js
+++ erpnext/smart_fm/doctype/building_environment_feedback/building_environment_feedback.js
@@ -2,7 +2,14 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Building Environment Feedback', {
-	// refresh: function(frm) {
-
-	// }
+	onload: function (frm) {
+		if (frm.is_new()) {
+		  frappe.db.get_value("User", frappe.session.user, ["email", "full_name", "phone", "mobile_no"])
+		  .then(r => {
+			let values = r.message;
+			frm.set_value("full_name", values.full_name);
+			frm.set_value("email_address", values.email);
+		  });
+		}
+	},
 });
