81fbf1b12d2eaf43dfbe8362c196b1d9629dae19
rizkyGP <139757784+rizkyGP@users.noreply.github.com>
2023-11-14 08:02:49 +0000

    add preview attachment

--- erpnext/smart_fm/web_form/maintenance_request_form/maintenance_request_form.js
+++ erpnext/smart_fm/web_form/maintenance_request_form/maintenance_request_form.js
@@ -1,3 +1,7 @@
 frappe.ready(function() {
 	// bind events here
+	frappe.web_form.on("attachment", ()=>{
+		frappe.web_form.render_image("preview", "attachment");
+	});
+	frappe.web_form.render_image("preview", "attachment");
 })
