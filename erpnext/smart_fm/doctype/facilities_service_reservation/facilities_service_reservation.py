230c46bb98318759c84c37eab822a61532702c35
rizkyGP <139757784+rizkyGP@users.noreply.github.com>
2023-11-15 07:50:22 +0000

    validate time

--- erpnext/smart_fm/doctype/facilities_service_reservation/facilities_service_reservation.py
+++ erpnext/smart_fm/doctype/facilities_service_reservation/facilities_service_reservation.py
@@ -1,8 +1,18 @@
 # Copyright (c) 2023, Frappe Technologies Pvt. Ltd. and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
+from frappe.utils import getdate, get_datetime
+from frappe import _
 
 class FacilitiesServiceReservation(Document):
-	pass
+	def validate(self):
+		self.validate_time()
+		self.validate_service()
+
+	def validate_time(self):
+		if get_datetime(self.from_time) > get_datetime(self.to_time):
+			frappe.throw(_("From time cannot higher than to time"))
+
+	
