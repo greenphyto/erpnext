853001b6191bbeab226a434927c2dba86a441be0
rizkyGP <139757784+rizkyGP@users.noreply.github.com>
2023-11-15 09:39:58 +0000

    validate qty when booking

--- erpnext/smart_fm/doctype/facilities_service_reservation/facilities_service_reservation.py
+++ erpnext/smart_fm/doctype/facilities_service_reservation/facilities_service_reservation.py
@@ -16,7 +16,50 @@ class FacilitiesServiceReservation(Document):
 			frappe.throw(_("From time cannot higher than to time"))
 
 	def validate_service(self):
-		return
-		frappe.db.sql("""
-				se
-				""")
+
+		if self.qty > self.quantity_available:
+			frappe.throw(_("This service is not avilable at your qty"))
+
+		# validate overlap
+		data = frappe.db.sql("""
+			select 
+				sum(qty) as qty, service
+			from 
+				`tabFacilities Service Reservation`
+			where 
+				docstatus = 1
+				and 
+					( 
+						( 
+							from_time >= %(from_time)s and from_time <= %(to_time)s
+						) or (
+							to_time >= %(from_time)s and to_time <= %(to_time)s
+						) or (
+							from_time <= %(from_time)s and to_time >= %(to_time)s
+						)
+					)
+				and service = %(service)s
+					   and name != %(name)s
+		""", {
+			"service":self.service,
+			"from_time":self.from_time,
+			"to_time":self.to_time,
+			"name":self.name
+		}, as_dict=1)
+
+		if data:
+			data = data[0]
+		
+			if data.qty:
+				available_qty_at_date = self.quantity_available - data.qty
+				if available_qty_at_date < self.qty:
+					frappe.throw(_("This service only available {} quantity at the date".format(available_qty_at_date)))
+
+	def on_submit(self):
+		doc = frappe.get_doc("Facility Service", self.service)
+		doc.set_rented(self.qty)
+
+	def on_cancel(self):
+		doc = frappe.get_doc("Facility Service", self.service)
+		doc.set_rented(self.qty, cancel=True)
+
