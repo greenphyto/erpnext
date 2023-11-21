b0e121ac7dcd67081912940fce85a66aa1b48eee
rizkyGP <139757784+rizkyGP@users.noreply.github.com>
2023-11-21 06:18:04 +0000

    set view facility service

--- erpnext/smart_fm/doctype/facilities_service_reservation/facilities_service_reservation.js
+++ erpnext/smart_fm/doctype/facilities_service_reservation/facilities_service_reservation.js
@@ -17,8 +17,22 @@ frappe.ui.form.on('Facilities Service Reservation', {
 	},
 	to_time: function(frm){
 		frm.cscript.set_total_duration();
+	},
+	refresh: function(frm){
+		frm.cscript.setup_preview();
+	},
+	all_day:function(frm){
+		frm.cscript.setup_preview();
+		frm.cscript.setup_value();
+	},
+	multi_days:function(frm){
+		frm.cscript.setup_value();
+		frm.cscript.setup_preview();
 	}
 });
+
+var START_TIME = '09:00';
+var END_TIME = '18:00';
 
 frappe.provide("cur_frm.cscript")
 $.extend(cur_frm.cscript, {
@@ -26,5 +40,40 @@ $.extend(cur_frm.cscript, {
 		var frm = this.frm;
 		var seconds = (moment(frm.doc.to_time) - moment(frm.doc.from_time)) /1000
 		frm.set_value("total_duration", seconds);
+	},
+	setup_preview(){
+		// if all day: hide time
+		var me = this;
+		var from_date = me.frm.fields_dict.from_date;
+		var to_date = me.frm.fields_dict.to_date;
+		var from_time = me.frm.fields_dict.from_time;
+		var to_time = me.frm.fields_dict.to_time;
+		if (me.frm.doc.all_day || me.frm.doc.multi_days){
+			me.frm.set_df_property("end_time", "hidden", 1);
+			me.frm.set_df_property("start_time", "hidden", 1);
+		}else{
+			me.frm.set_df_property("end_time", "hidden", 0);
+			me.frm.set_df_property("start_time", "hidden", 0);
+		}
+		
+		// if multi days: hide time
+		if (me.frm.doc.multi_days){
+			me.frm.set_df_property("to_date", "hidden", 0);
+			from_date.df.label = "From Date";
+			from_date.refresh();
+		}else{
+			me.frm.set_df_property("to_date", "hidden", 1);
+			from_date.df.label = "Date";
+			from_date.refresh();
+		}
+	},
+	setup_value: function(){
+		var me = this;
+		if (!this.frm.is_dirty()) return;
+
+		if (me.frm.doc.all_day || me.frm.doc.multi_days){
+			me.frm.set_value("start_time", START_TIME);
+			me.frm.set_value("end_time", END_TIME);
+		}
 	}
 })
