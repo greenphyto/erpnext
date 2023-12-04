f83f2dee1f7d52e53f487f73e6edd5f25e1683bc
rizkyGP <139757784+rizkyGP@users.noreply.github.com>
2023-12-04 10:54:47 +0000

    add precision

--- erpnext/accounts/report/asset_depreciations_and_balances/asset_depreciations_and_balances.js
+++ erpnext/accounts/report/asset_depreciations_and_balances/asset_depreciations_and_balances.js
@@ -39,6 +39,10 @@ frappe.query_reports["Asset Depreciations and Balances"] = {
 		}
 	],
 	"formatter": function(value, row, column, data, default_formatter) {
+		if (flt(value)>0 || value==0){
+			value = frappe.format(flt(value), {"fieldtype":"Currency", precision:2})
+		}
+
 		if (data.bold && value) {
 			value = $(`<span>${value}</span>`);
 			var $value = $(value).css("font-weight", "bold");
