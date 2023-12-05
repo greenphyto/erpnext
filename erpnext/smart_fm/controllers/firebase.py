51c04fa2c1fac2a1b6a99ab8c322edfd3b574424
rizkyGP <139757784+rizkyGP@users.noreply.github.com>
2023-12-05 10:49:56 +0000

    test message firebase

---
+++ erpnext/smart_fm/controllers/firebase.py
@@ -0,0 +1,23 @@
+import frappe
+
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import messaging
+
+class FirebaseNotification():
+    def __init__(self):
+        print(frappe.local.conf.firebase_key)
+        cred = credentials.Certificate(frappe.local.conf.firebase_key)
+        firebase_admin.initialize_app(cred)
+    
+    def send_message(self, text, token, title="Smart FM"):
+        message = messaging.Message(
+            data={
+                'title': title,
+                'body': text,
+            },
+            token=token,
+        )
+
+        # Mengirim pesan
+        response = messaging.send(message)
