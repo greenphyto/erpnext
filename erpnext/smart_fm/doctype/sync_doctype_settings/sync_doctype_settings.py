2115652845c740f3c7dc6d8ae7f893028233cdfd
rizkyGP <139757784+rizkyGP@users.noreply.github.com>
2023-12-27 09:06:24 +0000

    login and get pending log

--- erpnext/smart_fm/doctype/sync_doctype_settings/sync_doctype_settings.py
+++ erpnext/smart_fm/doctype/sync_doctype_settings/sync_doctype_settings.py
@@ -22,11 +22,32 @@ class SyncAPI():
 		return url
 	
 	def get_login(self):
+		if self.session.cookies.get("user_id"):
+			print(26)
+			return
+		
 		url = self.get_url("login")
 		res = self.session.post(url, data={
-			"usr": self.settings.api_key,
-			"pwd": self.settings.get_password("api_secret"),
+			"usr": self.settings.user,
+			"pwd": self.settings.get_password("password"),
 		})
 
 		data = res.json()
 		return data
+	
+	def req(self, req="POST", method="", data={}):
+		url = self.get_url(method)
+		self.get_login()
+		if req == "POST":
+			res = self.session.post(url, data=data)
+		else:
+			res = self.session.get(url, data=data)
+
+		result =  res.json()
+
+		return result.get("message") or {}
+
+	
+	def get_pending_log(self):
+		res = self.req("GET", "frappe.core.doctype.sync_log.sync_log.get_pending_log")
+		return res
