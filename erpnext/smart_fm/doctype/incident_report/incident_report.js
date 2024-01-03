8353fe920bd309439453a4f3ae455af0e199970b
rizkyGP <139757784+rizkyGP@users.noreply.github.com>
2024-01-03 07:36:25 +0000

    fix new trigger

--- erpnext/smart_fm/doctype/incident_report/incident_report.js
+++ erpnext/smart_fm/doctype/incident_report/incident_report.js
@@ -3,28 +3,30 @@
 
 frappe.ui.form.on('Incident Report', {
   onload: function (frm) {
-    frappe.db.get_value("User", frappe.session.user, ["email", "full_name", "phone", "mobile_no"])
-    .then(r => {
-      let values = r.message;
-      frm.set_value("name1", values.full_name);
-      frm.set_value("email_address", values.email);
-      frm.set_value("phone_number", values.phone);
-    });
-  },
+		if (frm.is_new()) {
+			frappe.db.get_value("User", frappe.session.user, ["email", "full_name", "phone", "mobile_no"])
+				.then(r => {
+					let values = r.message;
+					frm.set_value("name1", values.full_name);
+					frm.set_value("email_address", values.email);
+					frm.set_value("phone_number", values.phone);
+			});
+		}
+	},
 
 	refresh: function(frm) {
-    if (!frm.is_new()) {
-			frm.add_custom_button(__("ToDo"), function() {
-				frm.trigger("create_to_do");
-			}, __("Create"));
-    }
+		if (!frm.is_new()) {
+				frm.add_custom_button(__("ToDo"), function() {
+					frm.trigger("create_to_do");
+				}, __("Create"));
+		}
 	},
 
 	create_to_do: function(frm) {
 		frappe.call({
 			args: {
 				"name": frm.doc.name,
-      },
+			},
 			method: "erpnext.smart_fm.doctype.incident_report.incident_report.create_to_do",
 			callback: function(r) {
 				var doclist = frappe.model.sync(r.message);
