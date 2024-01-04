8f11a15ed9657baf7be179d1853c4c07b56bbd54
rizkyGP <139757784+rizkyGP@users.noreply.github.com>
2024-01-04 02:01:35 +0000

    update server

--- erpnext/patches/v13_0/fix_number_and_frequency_for_monthly_depreciation.py
+++ erpnext/patches/v13_0/fix_number_and_frequency_for_monthly_depreciation.py
@@ -5,7 +5,6 @@ def execute():
 	assets = frappe.get_all("Asset", filters={"allow_monthly_depreciation": 1})
 
 	for d in assets:
-		print(d.name)
 		asset_doc = frappe.get_doc("Asset", d.name)
 		for i in asset_doc.get("finance_books"):
 			if i.frequency_of_depreciation != 1:
