57cd5042075ef28f0b1bc10c22eabd21ac99cdfc
rizkyGP <139757784+rizkyGP@users.noreply.github.com>
2024-01-10 07:13:55 +0000

    redirect to asset for web access

--- erpnext/smart_fm/api.py
+++ erpnext/smart_fm/api.py
@@ -1,5 +1,6 @@
 import frappe, json
 from six import string_types
+from frappe.utils import get_url
 
 @frappe.whitelist(allow_guest=0)
 def qrcode():
@@ -18,9 +19,21 @@ def get_qrcode_data(code):
 
 		It will return JSON data
 	"""
+	accept_header = frappe.get_request_header("Accept") or ""
+	user_agent = frappe.get_request_header("User-Agent") or ""
+	respond_as_json = (
+		frappe.get_request_header("Accept")
+		and (frappe.local.is_ajax or "application/json" in accept_header)
+	)
+	if not respond_as_json:
+		web_access = ("Firefox", "Chrome", "Safari") 
+		respond_as_json = not any(elem in user_agent for elem in web_access)
 	
 	if frappe.db.exists("QRCode Data", code):
 		doc = frappe.get_doc("QRCode Data", code)
+		if not respond_as_json:
+			frappe.local.response["type"] = "redirect"
+			frappe.local.response["location"] = get_url("/app/asset/{}".format(doc.doc_name))
 		return doc.get_data()
 	else:
 		return {}
