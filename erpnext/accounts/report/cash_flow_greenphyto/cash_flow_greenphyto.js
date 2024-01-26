ac6df3d80bb265bdc10e2dbea79a061f083d3a7a
rizkyGP <139757784+rizkyGP@users.noreply.github.com>
2024-01-26 04:19:34 +0000

    get profit loss data

--- erpnext/accounts/report/cash_flow_greenphyto/cash_flow_greenphyto.js
+++ erpnext/accounts/report/cash_flow_greenphyto/cash_flow_greenphyto.js
@@ -7,3 +7,26 @@ frappe.query_reports["Cash Flow Greenphyto"] = {
 
 	]
 };
+
+
+frappe.require("assets/erpnext/js/financial_statements.js", function() {
+	frappe.query_reports["Cash Flow Greenphyto"] = $.extend({},
+		erpnext.financial_statements);
+
+	erpnext.utils.add_dimensions('Cash Flow Greenphyto', 10);
+
+	// The last item in the array is the definition for Presentation Currency
+	// filter. It won't be used in Cash Flow Greenphyto for now so we pop it. Please take
+	// of this if you are working here.
+
+	frappe.query_reports["Cash Flow Greenphyto"]["filters"].splice(8, 1);
+
+	frappe.query_reports["Cash Flow Greenphyto"]["filters"].push(
+		{
+			"fieldname": "include_default_book_entries",
+			"label": __("Include Default Book Entries"),
+			"fieldtype": "Check",
+			"default": 1
+		}
+	);
+});
--- erpnext/accounts/report/cash_flow_greenphyto/cash_flow_greenphyto.py
+++ erpnext/accounts/report/cash_flow_greenphyto/cash_flow_greenphyto.py
@@ -1,9 +1,24 @@
 # Copyright (c) 2024, Frappe Technologies Pvt. Ltd. and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
+from erpnext.accounts.report.profit_and_loss_statement.profit_and_loss_statement import execute as pl_report
+
 
 
 def execute(filters=None):
 	columns, data = [], []
+	pl_data = get_pl_report_data(filters=filters)
+	data = [pl_data]
 	return columns, data
+
+
+def get_pl_report_data(filters):
+	pl_data = pl_report(filters)
+	data = {}
+	if len(pl_data) > 1 and pl_data[1]:
+		for d in pl_data[1]:
+			if d.get("account"):
+				data[d['account']] = d
+
+	return data
