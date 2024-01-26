6624b513ae866e1f8ccb7d8d42e8e26e96c923f7
rizkyGP <139757784+rizkyGP@users.noreply.github.com>
2024-01-26 07:00:24 +0000

    setup columns

--- erpnext/accounts/report/cash_flow_greenphyto/cash_flow_greenphyto.py
+++ erpnext/accounts/report/cash_flow_greenphyto/cash_flow_greenphyto.py
@@ -4,17 +4,54 @@
 import frappe
 from erpnext.accounts.report.profit_and_loss_statement.profit_and_loss_statement import execute as pl_report
 from erpnext.accounts.report.balance_sheet_v2.balance_sheet_v2 import execute as bs_report
-
+from erpnext.accounts.report.financial_statements import (
+	get_columns,
+	get_period_list
+)
 
 def execute(filters=None):
 	columns, data = [], []
-	pl_data = get_pl_report_data(filters=filters)
 	
-	filters.accumulated_values = 1
-	bs_data = get_bs_report_data(filters=filters)
-	data = [bs_data]
-	return columns, data
+	return CashFlowReport(filters).run()
 
+class CashFlowReport():
+	def __init__(self, filters):
+		self.filters = filters
+		self.columns = []
+		self.data = []
+
+	def setup_report(self):
+		self.period_list = get_period_list(
+			self.filters.from_fiscal_year,
+			self.filters.to_fiscal_year,
+			self.filters.period_start_date,
+			self.filters.period_end_date,
+			self.filters.filter_based_on,
+			self.filters.periodicity,
+			company=self.filters.company,
+			month=self.filters.month,
+			to_month=self.filters.to_month,
+		)
+			
+	def setup_column(self):
+		self.columns = get_columns(
+			self.filters.periodicity, self.period_list, self.filters.accumulated_values, self.filters.company
+		)
+
+	def get_data(self):
+		self.data = []
+		pl_data = get_pl_report_data(filters=self.filters)
+	
+		self.filters.accumulated_values = 1
+		bs_data = get_bs_report_data(filters=self.filters)
+		self.data = [bs_data]
+
+	def run(self):
+		self.setup_report()
+		self.setup_column()
+		self.get_data()
+
+		return self.columns, self.data
 
 def get_pl_report_data(filters):
 	pl_data = pl_report(filters)
