77af2b6d97b63c1f2dcbe5b7d4b6380711ed849d
rizkyGP <139757784+rizkyGP@users.noreply.github.com>
2024-03-18 04:32:59 +0000

    post recipe init

--- erpnext/controllers/erp_api.py
+++ erpnext/controllers/erp_api.py
@@ -1,7 +1,22 @@
-import frappe
+import frappe, json
+from six import string_types
+from erpnext.controllers.foms import create_bom_products
 
 
 @frappe.whitelist()
 def ping_data(data):
 	print("GET DATA", data)
 	return data
+
+@frappe.whitelist()
+def post_recipe(data):
+	print(12, data)
+	if isinstance(data, string_types):
+		data = json.loads(data)
+		
+	data = frappe._dict(data)
+	product_id = data.get("productID")
+	
+	# create_bom_products(data, product_id )
+	
+	return product_id
