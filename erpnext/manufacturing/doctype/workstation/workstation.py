73df2aae93a4310e406e9a832642f9f557a38bee
rizkyGP <139757784+rizkyGP@users.noreply.github.com>
2024-05-21 09:33:43 +0000

    calculate workstation rate per qty

--- erpnext/manufacturing/doctype/workstation/workstation.py
+++ erpnext/manufacturing/doctype/workstation/workstation.py
@@ -34,6 +34,7 @@ class OverlapError(frappe.ValidationError):
 class Workstation(Document):
 	def before_save(self):
 		self.set_data_based_on_workstation_type()
+		self.validate_calculation_type()
 		self.set_hour_rate()
 
 	def set_hour_rate(self):
@@ -43,6 +44,25 @@ class Workstation(Document):
 			+ flt(self.hour_rate_consumable)
 			+ flt(self.hour_rate_rent)
 		)
+
+		self.per_qty_rate = (
+			flt(self.per_qty_rate_electricity)
+			+ flt(self.per_qty_rate_wages)
+			+ flt(self.per_qty_rate_machinery)
+		)
+
+	def validate_calculation_type(self):
+		if self.calculation_type == "Per Qty":
+			self.hour_rate = 0
+			self.hour_rate_labour = 0
+			self.hour_rate_electricity = 0
+			self.hour_rate_consumable = 0
+			self.hour_rate_rent = 0
+		else:
+			self.per_qty_rate = 0
+			self.per_qty_rate_electricity = 0
+			self.per_qty_rate_wages = 0
+			self.per_qty_rate_machinery = 0
 
 	@frappe.whitelist()
 	def set_data_based_on_workstation_type(self):
