8b31cccf7a26be428fc164145cdc1748af825fb7
rizkyGP <139757784+rizkyGP@users.noreply.github.com>
2024-05-31 07:36:29 +0000

    add current color list

--- erpnext/smart_fm/doctype/visitor_registration/visitor_registration_list.js
+++ erpnext/smart_fm/doctype/visitor_registration/visitor_registration_list.js
@@ -6,6 +6,8 @@ frappe.listview_settings['Visitor Registration'] = {
             "Sign In": [__("Sign In"), "green", "status,=,Sign In"],
             "Sign Out": [__("Sign Out"), "red", "status,=,Sign Out"],
             "Submitted": [__("Submitted"), "blue", "status,=,Submitted"],
+            "Pending": [__("Pending"), "orange", "status,=,Pending"],
+            "Accepted": [__("Accepted"), "blue", "status,=,Accepted"],
         }
          return status[doc.status];
 	},
