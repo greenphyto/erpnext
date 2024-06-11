3a02f574ffa7371e63e4c70a038e1465224072d9
rizkyGP <139757784+rizkyGP@users.noreply.github.com>
2024-06-11 07:32:12 +0000

    add list color

---
+++ erpnext/smart_fm/web_form/permit_to_work/permit_to_work_list.js
@@ -0,0 +1,34 @@
+
+
+frappe.custom_formatter_for_list = (fieldname, cell, value, doc)=>{
+
+    if (fieldname=='workflow_state'){
+        cell.addClass("status-cell");
+        return change_status_field(cell, value);
+    }else{
+        return cell;
+    }
+}
+
+function change_status_field(cell, status){
+    var place = $(cell).find('p');
+    place.text(status).removeClass();
+    place.addClass("indicator");
+    place.addClass("indicator-pill");
+    if (status=="Draft"){
+        place.addClass("gray");
+    }else if (status=="Sign In"){
+        place.addClass("green");
+    }else if (status=="Rejected"){
+        place.addClass("red");
+    }else if (status=="Submitted"){
+        place.addClass("blue");
+    }else if (status=="Sign Out"){
+        place.addClass("red");
+    }else if (status=="Approved"){
+        place.addClass("green");
+    }else{
+        place.addClass("gray");
+    }
+    return cell;
+}
