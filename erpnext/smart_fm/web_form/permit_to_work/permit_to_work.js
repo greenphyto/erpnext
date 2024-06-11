56161a61fa77edc15fd071e3739e679d2d4c5ae9
rizkyGP <139757784+rizkyGP@users.noreply.github.com>
2024-06-11 07:51:52 +0000

    fix not edit mode

--- erpnext/smart_fm/web_form/permit_to_work/permit_to_work.js
+++ erpnext/smart_fm/web_form/permit_to_work/permit_to_work.js
@@ -7,19 +7,21 @@ frappe.ready(function() {
 
 	var doc = frappe.web_form.doc;
 
-	if (doc.docstatus != 0){
+	if (doc.docstatus == 2){
 		$(".edit-button").hide();
 	}
 
-	var check_in_field = frappe.web_form.fields_dict.check_in_time;
-	var check_in = frappe.web_form.doc.check_in;
-	if (check_in_field && check_in==0){
-		check_in_field.set_value(frappe.datetime.now_datetime())
-	}
-	var check_out_field = frappe.web_form.fields_dict.check_out_time;
-	var check_out = frappe.web_form.doc.check_out;
-	if (check_out_field && check_out==0){
-		check_out_field.set_value(frappe.datetime.now_datetime())
+	if (frappe.web_form.in_edit_mode){
+		var check_in_field = frappe.web_form.fields_dict.check_in_time;
+		var check_in = frappe.web_form.doc.check_in;
+		if (check_in_field && check_in==0){
+			check_in_field.set_value(frappe.datetime.now_datetime())
+		}
+		var check_out_field = frappe.web_form.fields_dict.check_out_time;
+		var check_out = frappe.web_form.doc.check_out;
+		if (check_out_field && check_out==0){
+			check_out_field.set_value(frappe.datetime.now_datetime())
+		}
 	}
 })
 
