90a634c445710fedf064139297ead0c65c76b1a9
rizkyGP <139757784+rizkyGP@users.noreply.github.com>
2024-06-20 03:37:20 +0000

    add link

--- erpnext/buying/doctype/request/request.js
+++ erpnext/buying/doctype/request/request.js
@@ -1,13 +1,31 @@
 // Copyright (c) 2024, Frappe Technologies Pvt. Ltd. and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Request', {
-	quantity: function(frm) {
-    update_weight(frm)
-  },
-	packaging_size: function(frm) {
-    update_weight(frm)
-  },
+frappe.ui.form.on("Request", {
+	quantity: function (frm) {
+		update_weight(frm);
+	},
+	packaging_size: function (frm) {
+		update_weight(frm);
+	},
+	setup: function (frm) {
+		frm.set_query("department", (doc)=>{
+			return {
+				filters:{
+					is_group:0
+				}
+			}
+		})
+
+		frm.set_query("type_of_vegetable", (doc)=>{
+			return {
+				filters:{
+					item_group:"Products",
+					disabled:0
+				}
+			}
+		})
+	},
 });
 
 function update_weight(frm) {
