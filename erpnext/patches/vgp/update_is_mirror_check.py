6939bc8243da68fe532b287859d48206cd837b40
rizkyGP <139757784+rizkyGP@users.noreply.github.com>
2024-06-21 07:25:19 +0000

    update staging

--- erpnext/patches/vgp/update_is_mirror_check.py
+++ erpnext/patches/vgp/update_is_mirror_check.py
@@ -7,5 +7,4 @@ def update_is_mirror():
     maps = frappe.db.get_list("Sync Map", {"destination_doctype":"Asset"}, "destination_name")
     for d in maps:
         name = d.destination_name
-        print(name)
         frappe.db.set_value("Asset", name, "is_mirroring", 1)
