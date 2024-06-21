94a052d44e53906442fe64cab4dddd3fcf44b19d
rizkyGP <139757784+rizkyGP@users.noreply.github.com>
2024-06-21 04:11:52 +0000

    fix asset start date

--- erpnext/patches/v14_0/repair_grrenphyto_asset.py
+++ erpnext/patches/v14_0/repair_grrenphyto_asset.py
@@ -1,6 +1,6 @@
 import frappe
-from frappe.utils import getdate, get_last_day
-
+from frappe.utils import getdate, get_last_day,add_months
+from erpnext.assets.doctype.asset.depreciation import post_depreciation_entries
 """
 bench --site test4 execute erpnext.patches.v14_0.repair_grrenphyto_asset.fix_asset_gp
 """
@@ -9,6 +9,7 @@ def fix_asset_gp():
 	asset2 = "710011"
 	asset3 = "710015"
 
+	# post_depreciation_entries(date=getdate("2024-06-05"), asset_category=["ICT Hardware"])
 
 	# 1
 	def update_asset(asset_name, start_date):
@@ -55,10 +56,51 @@ def fix_asset_gp():
 			frappe.db.set_value("Asset Movement", exist, "transaction_date", data['transaction_date'])
 
 
-	# update_asset("720005-2", "2021-03-31" )
-	# update_asset("710011", "2022-04-14" )
-	# update_asset("710015", "2021-05-19" )
+	update_asset("720005-2", "2021-03-31" )
+	update_asset("710011", "2021-05-19" )
+	update_asset("710015", "2022-03-14" )
 	# frappe.throw("OKE")
 
 	# regenereate journal after fix start date
+	assets = ['720005-2', '710011', '710015']
+	regen_days = []
+	regen_jv = []
+	asset_category = []
+	data = frappe.db.sql("""
+		SELECT 
+			j.name, parent, a.name AS nm, j.posting_date
+		FROM
+			`tabJournal Entry Account` a
+				LEFT JOIN
+			`tabJournal Entry` j ON j.name = a.parent
+		WHERE
+			a.reference_type = 'Asset'
+				AND a.reference_name in %(data)s
+				AND a.docstatus = 1
+					  """, {"data":assets}, as_dict=1)
+	# 1. find other JV
+	for d in assets:
+		doc = frappe.get_doc("Asset", d)
+		if doc.asset_category not in asset_category:
+			asset_category.append(doc.asset_category)
+
+		used_jv = [x.journal_entry for x in  doc.get("schedules") if x.journal_entry]
+		for jv in data:
+			if jv.name not in used_jv and jv.name not in regen_jv:
+				regen_jv.append(jv.name)
+				if jv.posting_date not in regen_days:
+					regen_days.append(jv.posting_date)
 	
+	print("Need regen")
+	print(regen_jv)
+	print(regen_days)
+
+	for d in regen_jv:
+		doc = frappe.get_doc("Journal Entry", d)
+		doc.cancel()
+	
+	# 2. regenerate others
+	for d in regen_days:
+		post_depreciation_entries(date=add_months(d, 1), asset_category=asset_category)
+
+	# post_depreciation_entries(date="2023-11-30", asset_category=asset_category)
