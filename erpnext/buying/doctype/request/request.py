a6d7711415c16b1c7b98e8c0a8635d193a0a0990
rizkyGP <139757784+rizkyGP@users.noreply.github.com>
2024-06-26 08:45:24 +0000

    validate value request

--- erpnext/buying/doctype/request/request.py
+++ erpnext/buying/doctype/request/request.py
@@ -1,8 +1,30 @@
 # Copyright (c) 2024, Frappe Technologies Pvt. Ltd. and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
+from frappe.utils import getdate, flt
+from frappe import _
 
 class Request(Document):
-	pass
+	def validate(self):
+		self.calculate_price()
+		self.calculate_weight()
+		self.validate_date()
+
+	def validate_date(self):
+		if getdate(self.delivery_date) < getdate(self.posting_date):
+			frappe.throw(_("Delivery Date cannot before posting date."))
+
+	def calculate_price(self):
+		self.total_price = 0
+		for d in self.get("items"):
+			d.amount = flt(d.rate) * flt(d.qty)
+			self.total_price += d.amount
+
+	def calculate_weight(self):
+		self.total_weight = 0
+		for d in self.get("items"):
+			d.weight = flt(d.unit_weight) * flt(d.qty)
+			self.total_weight += d.weight
+
