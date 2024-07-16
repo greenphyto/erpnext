d53bc865a2a24e3b935c3d8cd2a18098d7468628
rizkyGP <139757784+rizkyGP@users.noreply.github.com>
2024-07-16 06:26:40 +0000

    update current material number

--- erpnext/patches/v14_0/repair_item.py
+++ erpnext/patches/v14_0/repair_item.py
@@ -1,10 +1,10 @@
 import frappe
 
 """
-bench --site test3 execute erpnext.patches.v14_0.repair_item.add_material_number_products
+bench --site test3 execute erpnext.patches.v14_0.repair_item.add_material_number_rawmat
 """
 # update products material group
-def add_material_number_products():
+def add_material_number_rawmat():
     data = frappe.get_all("Item", {
         "material_group":['is', 'not set', ],
         "item_group": "Raw Material"
@@ -25,5 +25,28 @@ def add_material_number_products():
             left_items.append(d)
     for d in left_items:
         print("left for", d.name, ",", d.item_name)
-        
+
+"""
+bench --site test3 execute erpnext.patches.v14_0.repair_item.add_material_number_products
+"""
 # update raw material group
+def add_material_number_products():
+    data = frappe.get_all("Item", {
+        "material_group":['is', 'not set'],
+        "item_group": "Products"
+    }, ['name', 'item_name'])
+    left_items = []
+    for d in data:
+        if "AV" in d.name:
+            print("Process AV", d.name, d.item_name)
+            doc = frappe.get_doc("Item", d.name)
+            doc.material_group = 'Vegetables (Asian Vegetables)'
+            doc.save()
+        elif "LV" in d.name:
+            print("Process LV", d.name, d.item_name)
+            doc = frappe.get_doc("Item", d.name)
+            doc.material_group = 'Vegetables (Lettuce)'
+            doc.save()
+    
+    for d in left_items:
+        print("left for", d.name, ",", d.item_name)
