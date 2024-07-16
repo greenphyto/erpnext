8b62926a3677b7ceb79cc8fe723db02efed14ca5
rizkyGP <139757784+rizkyGP@users.noreply.github.com>
2024-07-16 04:59:21 +0000

    update raw material group

---
+++ erpnext/patches/v14_0/repair_item.py
@@ -0,0 +1,29 @@
+import frappe
+
+"""
+bench --site test3 execute erpnext.patches.v14_0.repair_item.add_material_number_products
+"""
+# update products material group
+def add_material_number_products():
+    data = frappe.get_all("Item", {
+        "material_group":['is', 'not set', ],
+        "item_group": "Raw Material"
+    }, ['name', 'item_name'])
+    left_items = []
+    for d in data:
+        if "seed" in d.item_name.lower():
+            print("Process seeds", d.name, d.item_name)
+            doc = frappe.get_doc("Item", d.name)
+            doc.material_group = "Seeds"
+            doc.save()
+        elif any(n in d.name for n in ['NSB', 'NSA', 'NPB', 'NPA']):
+            print("Process nutrient", d.name, d.item_name)
+            doc = frappe.get_doc("Item", d.name)
+            doc.material_group = "Nutrition"
+            doc.save()
+        else:
+            left_items.append(d)
+    for d in left_items:
+        print("left for", d.name, ",", d.item_name)
+        
+# update raw material group
