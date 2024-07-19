088188abcc091c89047c2a851d96cd165f08cce7
rizkyGP <139757784+rizkyGP@users.noreply.github.com>
2024-07-19 06:21:36 +0000

    add other material

--- erpnext/patches/v14_0/fix_part_number.py
+++ erpnext/patches/v14_0/fix_part_number.py
@@ -1,11 +1,9 @@
 import frappe
+from frappe.utils import cstr
 
 def execute():
-    fix_raw_material()
-
-def fix_raw_material():
-    print(frappe.local.conf)
-    data = {
+    # Seeds
+    do_sync({
         "RM-SD-CR": "100000",
         "RM-SD-CS": "100001",
         "RM-SD-WC": "100002",
@@ -16,24 +14,84 @@ def fix_raw_material():
         "RM-SD-NB": "100007",
         "RM-SD-PC": "100008",
         "RM-SD-KL": "100009"
-    }
+    }, "Seeds", 10, 10)
+
+    # Veg (AV)
+    do_sync({
+        "PR-AV-CS":"150000",
+        "PR-AV-WC":"150001",
+        "PR-AV-GN":"150002",
+        "PR-AV-NB":"150003",
+        "PR-AV-PC":"150004",
+        "PR-AV-KL":"150005",
+    }, "Vegetables (Asian Vegetables)", 15, 6)
+
+    # Veg (LV)
+    do_sync({
+        "PR-LV-CR":"140000",
+        "PR-LV-CC":"140001",
+        "PR-LV-GM":"140002",
+        "PR-LV-RB":"140003",
+    }, "Vegetables (Lettuce)", 14, 4)
+
+    # Nutrient
+    do_sync({
+        "RM-NS-NSA":"110000",
+        "RM-NS-NSB":"110001",
+    }, "Nutrition", 11, 2)
+
+    # Other Packaging
+    do_sync({
+        "ZOT01":"130000",
+        "ZOT02":"130001",
+        "ZOT03":"130002",
+    }, "Other Packaging", 13, 3)
+
+    # LED
+    do_sync({
+        "PDLED01":"200000",
+        "PDLED02":"200001",
+    }, "LED", 20, 2)
+
+    # Gateway
+    do_sync({
+        "ZGW01 ":"210000"
+    }, "Gateway", 21, 1)
+
+    # Dimmer Controller
+    do_sync({
+        "DMC01 ":"220000"
+    }, "Gateway", 22, 1)
+
+    # FG Systems
+    do_sync({
+        "ZMSC01 ":"260000",
+        "ZMSC02 ":"260001",
+    }, "Gateway", 22, 2)
+
+
+def do_sync(data, material_group, series_key, start_from):
 
     data_reff = list(data.keys())
     print(data_reff)
 
-    num_series = 10
+    idx = start_from
+    num_series = series_key * 10000
     for d in frappe.db.get_all("Item", {'name':['in', data_reff]}, ['name', 'material_group', 'material_number']):
         new_series = data.get(d.name)
         frappe.db.set_value("Item", d.name, "material_number", new_series)
         print(d, new_series)
 
     print("Update all")
-    for d in frappe.db.get_all("Item", {"material_group":"Seeds", 'name':['not in', data_reff]}, order_by='creation'):
-        new_series = 100000 + num_series
-        num_series += 1
+    for d in frappe.db.get_all("Item", {"material_group": material_group, 'name':['not in', data_reff]},['name', 'material_group', 'material_number'], order_by='creation'):
+        new_series = num_series + idx
+        idx += 1
         frappe.db.set_value("Item", d.name, "material_number", new_series)
         print(d, new_series)
 
-    print("Now", num_series-1)
-    frappe.db.sql('update `tabSeries` set current = {} where name = "10" '.format(num_series-1))
+    if idx > start_from:
+        idx -= 1
+
+    print("Now", idx)
+    frappe.db.sql('update `tabSeries` set current = {} where name = %s '.format(idx), cstr(series_key))
 
