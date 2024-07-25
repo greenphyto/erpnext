4c8bf9e57030e7435565b2bf02676d594fe0c18e
rizkyGP <139757784+rizkyGP@users.noreply.github.com>
2024-07-25 10:24:03 +0000

    collect expiry items

--- erpnext/stock/doctype/scrap_request/scrap_request.py
+++ erpnext/stock/doctype/scrap_request/scrap_request.py
@@ -4,6 +4,7 @@
 import frappe
 from frappe.model.document import Document
 from frappe.desk.reportview import get_filters_cond, get_match_cond
+from frappe.utils import getdate, add_days
 
 class ScrapRequest(Document):
 	pass
@@ -29,3 +30,28 @@ def get_batch_numbers(doctype, txt, searchfield, start, page_len, filters):
 
 	query += " order by expiry_date asc"
 	return frappe.db.sql(query, filters)
+
+def collect_expired_items():
+	enable, within_days = frappe.db.get_value("Stock Settings","Stock Settings", ['enable_auto_collect_expired_items', 'expiry_days']) or 0, 0
+
+	if not enable:
+		return
+	
+	use_date = add_days(getdate(), within_days)
+
+	# get data
+	data = frappe.db.get_all("Batch", {"expiry_date": ['<', use_date], "batch_qty":['>', 0]}, ['name', 'item', 'batch_qty'])
+	if not data:
+		return
+
+	# create scrap request
+	doc = frappe.new_doc("Scrap Request")
+	for d in data:
+		row = doc.append("items")
+		row.item_code = d.item
+		row.batch = d.batch
+		row.qty = d.batch_qty
+		# warehouse
+	doc.reason = "Expired item"
+	doc.insert(ignore_permissions=1)
+	return doc.name
