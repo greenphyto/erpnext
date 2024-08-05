bf148c05db014366c0dc715d77ffb92909460608
rizkyGP <139757784+rizkyGP@users.noreply.github.com>
2024-08-05 03:21:37 +0000

    sync UOM with package

--- erpnext/stock/doctype/packaging/packaging.py
+++ erpnext/stock/doctype/packaging/packaging.py
@@ -1,8 +1,45 @@
 # Copyright (c) 2024, Frappe Technologies Pvt. Ltd. and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
+from frappe.utils import cint
 
 class Packaging(Document):
-	pass
+	def validate(self):
+		self.create_uom_packaging()
+		self.enable_disable()
+
+	def on_trash(self):
+		self.delete_uom_packaging()
+
+	def create_uom_packaging(self):
+		if self.get_uom_name():
+			return
+	
+		doc = frappe.new_doc("UOM")
+		doc.is_packaging = 1
+		doc.must_be_whole_number = 1
+		doc.uom_name = self.name
+		doc.insert(ignore_permissions=1)
+
+	def get_uom_name(self):
+		return frappe.get_value("UOM", self.name)
+
+	def delete_uom_packaging(self):
+		exists = self.get_uom_name()
+		if not exists:
+			return
+	
+		frappe.delete_doc("UOM", exists)
+
+	def enable_disable(self):
+		exists = self.get_uom_name()
+		if not exists:
+			return
+		
+		if cint(self.disable):
+			frappe.db.set_value("UOM", exists, "enabled", 0)
+		else:
+			frappe.db.set_value("UOM", exists, "enabled", 1)
+
