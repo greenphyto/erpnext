5f9a1cca03f789c791dc9d8d237c1d790545df29
Eileen <84365588+eileengreenphyto@users.noreply.github.com>
2024-08-07 09:05:21 +0000

    bms alarm changes

--- erpnext/foms/doctype/bms_alarm/bms_alarm.js
+++ erpnext/foms/doctype/bms_alarm/bms_alarm.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2024, Frappe Technologies Pvt. Ltd. and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('FOMS Alarm', {
+frappe.ui.form.on('BMS Alarm', {
 	// refresh: function(frm) {
 
 	// }
--- erpnext/foms/doctype/bms_alarm/test_bms_alarm.py
+++ erpnext/foms/doctype/bms_alarm/test_bms_alarm.py
@@ -5,5 +5,5 @@
 from frappe.tests.utils import FrappeTestCase
 
 
-class TestFOMSAlarm(FrappeTestCase):
+class TestBMSAlarm(FrappeTestCase):
 	pass
