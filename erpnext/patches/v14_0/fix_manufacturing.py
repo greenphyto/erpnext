b87e4763a9bb9415c0cfd714165ebba3959c4fb6
rizkyGP <139757784+rizkyGP@users.noreply.github.com>
2024-08-07 07:51:34 +0000

    update valuation rate

--- erpnext/patches/v14_0/fix_manufacturing.py
+++ erpnext/patches/v14_0/fix_manufacturing.py
@@ -2,6 +2,7 @@ import frappe
 
 def execute():
     update_bom_to_against_job_card()
+    update_valuation_rate()
 
 def update_bom_to_against_job_card():
     frappe.db.sql("""
@@ -10,4 +11,12 @@ def update_bom_to_against_job_card():
             transfer_material_against = 'Job Card'
         WHERE
             transfer_material_against = 'Work Order'           
+    """)
+def update_valuation_rate():
+    frappe.db.sql("""
+        UPDATE `tabItem` 
+            SET 
+                valuation_rate = 1
+            WHERE
+                valuation_rate = 0
     """)
