e5fcac865368d318b2a5fe12d67f18150fb7e572
rizkyGP <139757784+rizkyGP@users.noreply.github.com>
2024-08-09 11:10:32 +0000

    fix status as draft

--- erpnext/assets/doctype/asset_repair/asset_repair_list.js
+++ erpnext/assets/doctype/asset_repair/asset_repair_list.js
@@ -1,12 +1,12 @@
 frappe.listview_settings['Asset Repair'] = {
-	add_fields: ["repair_status"],
+	add_fields: ["repair_status", "docstatus"],
 	get_indicator: function(doc) {
-		if(doc.repair_status=="Pending") {
-			return [__("Pending"), "orange"];
-		} else if(doc.repair_status=="Completed") {
+		if(doc.repair_status=="Completed" && doc.docstatus==1) {
 			return [__("Completed"), "green"];
 		} else if(doc.repair_status=="Cancelled") {
 			return [__("Cancelled"), "red"];
+		} else {
+			return [__("Pending"), "orange"];
 		}
 	}
 };
