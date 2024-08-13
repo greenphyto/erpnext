693fea0652895af70b2f2f69878fcf8cc9a63bdb
Eileen <84365588+eileengreenphyto@users.noreply.github.com>
2024-08-13 06:03:20 +0000

    building env feedback form changes

--- erpnext/smart_fm/doctype/building_environment_feedback/building_environment_feedback.js
+++ erpnext/smart_fm/doctype/building_environment_feedback/building_environment_feedback.js
@@ -3,13 +3,13 @@
 
 frappe.ui.form.on('Building Environment Feedback', {
 	onload: function (frm) {
-		if (frm.is_new()) {
-		  frappe.db.get_value("User", frappe.session.user, ["email", "full_name", "phone", "mobile_no"])
-		  .then(r => {
-			let values = r.message;
-			frm.set_value("full_name", values.full_name);
-			frm.set_value("email_address", values.email);
-		  });
-		}
+		// if (frm.is_new()) {
+		//   frappe.db.get_value("User", frappe.session.user, ["email", "full_name", "phone", "mobile_no"])
+		//   .then(r => {
+		// 	let values = r.message;
+		// 	frm.set_value("full_name", values.full_name);
+		// 	frm.set_value("email_address", values.email);
+		//   });
+		// }
 	},
 });
