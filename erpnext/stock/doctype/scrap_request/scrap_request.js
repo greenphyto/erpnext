9f072e99d437e0081e2dc4d7795765edb04fc9f8
rizkyGP <139757784+rizkyGP@users.noreply.github.com>
2024-08-23 02:46:40 +0000

    fix ui scrap request

--- erpnext/stock/doctype/scrap_request/scrap_request.js
+++ erpnext/stock/doctype/scrap_request/scrap_request.js
@@ -2,15 +2,13 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Scrap Request', {
-	item: function(frm) {
-		update_quantity(frm)
-	},
-	warehouse: function(frm) {
-		update_quantity(frm)
-	},
 	refresh: function(frm) {
 		frm.set_query("batch", "items", (doc, cdt,cdn)=>{
 			var d = locals[cdt][cdn];
+
+			if (!d.item_code){
+				frappe.throw(__("Please select Item."))
+			}
 			return {
 				filters:{
 					item:d.item_code
@@ -18,45 +16,13 @@ frappe.ui.form.on('Scrap Request', {
 				query:"erpnext.stock.doctype.scrap_request.scrap_request.get_batch_numbers"
 			}
 		})
-
-		frm.set_query("warehouse", "items", (doc, cdt,cdn)=>{
-			var d = locals[cdt][cdn];
-			return {
-				filters:{
-					item:d.item_code,
-					batch:d.batch
-				},
-				query:"erpnext.stock.doctype.scrap_request.scrap_request.get_warehouse"
-			}
-		})
 	}
 });
 
 frappe.ui.form.on('Scrap Items', {
 	item_code: function(frm,cdt,cdn){
-		update_quantity(cdt,cdn)
-	},
-	warehouse: function(frm,cdt,cdn){
-		update_quantity(cdt,cdn)
+		frappe.model.set_value(cdt,cdn, "batch", "");
+		frappe.model.set_value(cdt,cdn, "cur_qty", "");
+		frappe.model.set_value(cdt,cdn, "uom", "");
 	}
 })
-
-function update_quantity(cdt,cdn) {
-	var d = locals[cdt][cdn];
-	if (d.item_code && d.warehouse) {
-		frappe.call({
-			method: "erpnext.accounts.doctype.pos_invoice.pos_invoice.get_stock_availability",
-			args: {
-				'item_code': d.item_code,
-				'warehouse': d.warehouse,
-			},
-			callback: function(r) {
-				if(r.message) {
-					frappe.model.set_value(cdt,cdn, "cur_qty", r.message[0])
-				}
-			}
-		});
-	}else{
-		frappe.model.set_value(cdt,cdn, "cur_qty", 0)
-	}
-}
