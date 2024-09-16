8012f2b59697f4a1c451ed0194ed399a5a3d5514
rizkyGP <139757784+rizkyGP@users.noreply.github.com>
2024-09-16 16:44:49 +0000

    new version button

--- erpnext/manufacturing/doctype/workstation/workstation.js
+++ erpnext/manufacturing/doctype/workstation/workstation.js
@@ -30,6 +30,13 @@ frappe.ui.form.on("Workstation", {
 
 	refresh: function(frm){
 		frm.cscript.change_label();
+
+		if (!frm.is_new() && !frm.doc.docstatus == 0) {
+			frm.add_custom_button(__("New Version"), function() {
+				let new_bom = frappe.model.copy_doc(frm.doc);
+				frappe.set_route("Form", "Workstation", new_bom.name);
+			});
+		}
 	},
 
 	workstation_type: function(frm) {
