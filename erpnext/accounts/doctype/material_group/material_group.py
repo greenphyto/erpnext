628c6b9b01bc0ccd358757980a16d35036859acc
rizkyGP <139757784+rizkyGP@users.noreply.github.com>
2024-09-23 07:19:19 +0000

    check overlaping value

--- erpnext/accounts/doctype/material_group/material_group.py
+++ erpnext/accounts/doctype/material_group/material_group.py
@@ -7,9 +7,44 @@ from frappe import _
 from frappe.model.document import Document
 
 class MaterialGroup(Document):
-	def validate(self):
-		self.valdiate_number_range()
+	def onload(self):
+		self.check_exist_item()
 
-	def valdiate_number_range(self)
+	def validate(self):
+		self.validate_number_range()
+		self.validate_number_exists()
+
+	def validate_number_range(self):
 		if cint(self.number_end) < cint(self.number_start):
 			frappe.throw(_("Number end must be grater than start"))
+	
+	def validate_number_exists(self):
+		filters = [
+			{
+				"number_start":['between', [self.number_start, self.number_end]],
+				"name":['!=', self.name]
+			},
+			{
+				"number_end":['between', [self.number_start, self.number_end]],
+				"name":['!=', self.name]
+			},
+			{
+				"number_start":['>=', self.number_start],
+				"number_end":['<=', self.number_end],
+				"name":['!=', self.name]
+			},
+			{
+				"number_start":['<=', self.number_start],
+				"number_end":['>=', self.number_end],
+				"name":['!=', self.name]
+			},
+		]
+
+		for f in filters:
+			exist = frappe.db.get_value("Material Group", f, ['material_group_name','number_start', 'number_end' ], as_dict=1)
+
+			if exist:
+				frappe.throw(_(f"overlapping number range with <b>{exist.material_group_name}</b> ({exist.number_start}-{exist.number_end})"))
+
+	def check_exist_item(self):
+		self.set_onload("exist_transaction", False)
