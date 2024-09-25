c698fd44e6467535f3c27a639ad669f23222e1d6
rizkyGP <139757784+rizkyGP@users.noreply.github.com>
2024-09-25 11:24:05 +0000

    load items

--- erpnext/accounts/doctype/part_number_settings/part_number_settings.js
+++ erpnext/accounts/doctype/part_number_settings/part_number_settings.js
@@ -2,7 +2,16 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Part Number Settings', {
-	// refresh: function(frm) {
-
-	// }
+	refresh: function(frm) {
+		frm.add_custom_button(__('Get Items'), () => {
+			frappe.call({
+				method:"load_items",
+				doc:frm.doc,
+				callback: function(r){
+					frm.refresh();
+					frm.dirty();
+				}
+			})
+		})
+	}
 });
