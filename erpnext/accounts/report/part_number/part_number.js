e77420bf311622ddeba470cdd9414e8daec4d189
rizkyGP <139757784+rizkyGP@users.noreply.github.com>
2024-09-25 09:28:48 +0000

    add button to edit settings

--- erpnext/accounts/report/part_number/part_number.js
+++ erpnext/accounts/report/part_number/part_number.js
@@ -3,5 +3,14 @@
 /* eslint-disable */
 
 frappe.query_reports["Part Number"] = {
-	
+	onload: function(report){
+		report.page.add_button(__("Edit Settings"), function() {
+			frappe.set_route('Form', 'Part Number Settings');
+		}, {
+			btn_class: "btn-primary btn-edit-settings"
+		});
+		$(report.page.parent).append(`
+			<style> .custom-actions > button:not(.btn-edit-settings) { display: none; }</style>
+		`)
+	}
 };
