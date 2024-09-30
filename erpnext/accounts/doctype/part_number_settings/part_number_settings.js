c1a81c0d33191b99b58ff6ee4bc132b36594f77e
rizkyGP <139757784+rizkyGP@users.noreply.github.com>
2024-09-30 03:39:12 +0000

    edit all for some material group

--- erpnext/accounts/doctype/part_number_settings/part_number_settings.js
+++ erpnext/accounts/doctype/part_number_settings/part_number_settings.js
@@ -13,5 +13,22 @@ frappe.ui.form.on('Part Number Settings', {
 				}
 			})
 		})
+
+		frm.add_custom_button(__('Edit All'), () => {
+			frappe.prompt([
+				{'fieldname': 'material_group', 'fieldtype': 'Link', 'label': 'Material Group', 'reqd': 1, "options":"Material Group"},
+				{'fieldname': 'account_code', 'fieldtype': 'Link', 'label': 'Account Code', 'reqd': 1, "options":"Account"}
+			],
+			function(r){
+				$.each(frm.doc.data_mapping, (i, d)=>{
+					if (r.material_group==d.material_group){
+						frappe.model.set_value(d.doctype, d.name, "account_code", r.account_code);
+					}
+				})
+			},
+			'Update all account code',
+			'Submit'
+			)
+		})
 	}
 });
