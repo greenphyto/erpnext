ed956c36d36b103dda931021f3b1d3fff606d4b7
rizkyGP <139757784+rizkyGP@users.noreply.github.com>
2024-10-28 10:40:09 +0000

    add JS filter

--- erpnext/manufacturing/report/cos_product_variances/cos_product_variances.js
+++ erpnext/manufacturing/report/cos_product_variances/cos_product_variances.js
@@ -4,6 +4,37 @@
 
 frappe.query_reports["COS Product Variances"] = {
 	"filters": [
-
+		{
+			"fieldname":"from_date",
+			"label": __("From Date"),
+			"fieldtype": "Date",
+			"default": frappe.datetime.add_months(new Date(), -1)
+		},
+		{
+			"fieldname":"to_date",
+			"label": __("To Date"),
+			"fieldtype": "Date",
+			"default": "Today"
+		},
+		{
+			"fieldname":"product",
+			"label": __("Product"),
+			"fieldtype": "Link",
+			"options": "Item",
+			"get_query":()=>{
+				return {
+					filters: {
+						foms_product_id:['is', 'set'],
+						disabled:0
+					}
+				}
+			}
+		},
+		{
+			"fieldname":"work_order",
+			"label": __("Work Order"),
+			"fieldtype": "Link",
+			"options": "Work Order"
+		}
 	]
 };
