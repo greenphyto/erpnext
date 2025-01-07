b010131cd2d9ace1011e599438fe74596724e7b1
rizkyGP <139757784+rizkyGP@users.noreply.github.com>
2025-01-07 04:30:57 +0000

    fix missing default cost center

--- erpnext/accounts/utils.py
+++ erpnext/accounts/utils.py
@@ -1809,6 +1809,10 @@ def get_cost_center_from_account(account, company=""):
 	if not company:
 		company = erpnext.get_default_company()
 
+	default_cost_center = erpnext.get_default_cost_center(company)
+	if not account:
+		return {"value":default_cost_center, "lock":0}
+
 	cost_center = frappe.get_value("Cost Center Mapping", {"company":company, "account":account}, "cost_center")
 	if cost_center:
 		return {"value":cost_center, "lock":1}
