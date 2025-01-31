8361eb827e82aaa0eea0e88366fb79ff28ffd85b
rizkyGP <139757784+rizkyGP@users.noreply.github.com>
2025-01-31 09:21:18 +0000

    add item and rate on report column

---
+++ erpnext/buying/doctype/request/request_list.js
@@ -0,0 +1,7 @@
+frappe.listview_settings['Request'] = {
+	add_fields: [
+        {fieldname:"item_code", parent:"Request Items"},
+        {fieldname:"rate", parent:"Request Items"},
+        {fieldname:"uom", parent:"Request Items"},
+    ]
+}
