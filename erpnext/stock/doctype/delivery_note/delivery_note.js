7bbe52f3bd4d4035262b920dad9f4f3a184f80be
rizkyGP <139757784+rizkyGP@users.noreply.github.com>
2025-02-19 02:29:21 +0000

    ignore attachment if not have

--- erpnext/stock/doctype/delivery_note/delivery_note.js
+++ erpnext/stock/doctype/delivery_note/delivery_note.js
@@ -458,6 +458,10 @@ erpnext.add_image_slide = function(frm){
     </div>
 	`)
 
+	if (is_null(frm.doc.attachment)){
+		return
+	}
+
 	$.each(frm.doc.attachment.split(";"), (i, v)=>{
 		content.find(".splide__list").append(`
 			<li class="splide__slide">
