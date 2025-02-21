c1e19ffcd473f4b95e919e2c2c8ff34d492484b3
rizkyGP <139757784+rizkyGP@users.noreply.github.com>
2025-02-21 10:03:17 +0000

    make calendar on desk site

--- erpnext/smart_fm/doctype/tour_protocol_checklist/tour_protocol_checklist_calendar.js
+++ erpnext/smart_fm/doctype/tour_protocol_checklist/tour_protocol_checklist_calendar.js
@@ -14,12 +14,24 @@ frappe.views.calendar["Tour Protocol Checklist"] = {
 		slotMinTime: "06:00:00",
 		slotMaxTime: "18:30:00",
 		axisFormat: 'HH:mm',
+		eventRender: (info, el)=>{
+			console.log(info, el)
+        	var el = $(el);
+        	var prop = info
+        	var vip = "";
+			if (prop.vip_status=="Yes"){
+				vip =`<span class="vip-title">VIP</span>`
+			}
+			el.find(".fc-title").html(`<div>${ prop.title } ${vip}<div>`);
+			el.find(".fc-content").append(`<div>${prop.participants || 1} Person<div>`)
+			el.find(".fc-content").append(`<div>IC: ${prop.full_name || prop.tour_ic || "-"}<div>`)
+		}
     },
 	get_events_method: "erpnext.smart_fm.doctype.tour_protocol_checklist.tour_protocol_checklist.get_events",
 	hide_sidebar: true,
 	before_render: (calendar)=>{
 		// calendar.custom = new TourCards(calendar);
-	}
+	},
 };
 
 
