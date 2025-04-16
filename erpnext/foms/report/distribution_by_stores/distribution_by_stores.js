5ba1edfcb917f4d0a4a6bb4ef23f689ed5f2575d
rizkyGP <139757784+rizkyGP@users.noreply.github.com>
2025-04-16 10:52:22 +0000

    freeze column and data

--- erpnext/foms/report/distribution_by_stores/distribution_by_stores.js
+++ erpnext/foms/report/distribution_by_stores/distribution_by_stores.js
@@ -24,5 +24,45 @@ frappe.query_reports["Distribution by Stores"] = {
 
 		frappe.query_report.set_filter_value("start_date", startOfMonth);
 		frappe.query_report.set_filter_value("end_date", endOfMonth);
-	}
+
+	},
+	get_datatable_options: function(options){
+		options.inlineFilters = false;
+		return options
+	},
+	formatter:function(value, row, column, data, default_formatter) {
+		if (row.meta.rowIndex==0){
+			value = `<div style='text-align: center'>${value || ""}</div>`
+		} else if (column.colIndex==1){
+			value = `<div style='text-align: center'>${value || ""}</div>`
+		} else{
+			value = default_formatter(value, row, column, data);
+		}
+		return value;
+	},
+	after_datatable_render: function(dt){
+		dt.style.setStyle(".dt-cell--col-0", {"display":"none !important"})
+		dt.style.setStyle(".dt-cell--col-1", {"position":"sticky", "left":0, "z-index":1})
+		dt.style.setStyle(".dt-cell--col-2", {"position":"sticky", "left":"50px", "z-index":1, "border-right":"1px solid #bcbcbc", "background-color":"white"})
+		dt.style.setStyle(".dt-row-0", {
+			"z-index": 2,
+			"border-bottom": "1px solid #bcbcbc",
+			"background-color":"white"
+		})
+
+
+		var scrollBody = dt.wrapper.querySelector(".dt-scrollable");
+
+		if (scrollBody) {
+			scrollBody.addEventListener("scroll", (e) => {
+			const scrollTop = e.target.scrollTop;
+			const scrollLeft = e.target.scrollLeft;
+			var new_pos = scrollLeft + 50;
+			dt.style.setStyle(".dt-row-header > .dt-cell--col-1", {"transform":`translateX(${scrollLeft}px)`})
+			dt.style.setStyle(".dt-row-header > .dt-cell--col-2", {"transform":`translateX(${scrollLeft}px)`})
+			dt.style.setStyle(".dt-row-0", {"transform":`translateY(${scrollTop}px)`})
+			});
+		}
+	},
+	
 };
