b3ccc4806b1f814c1673434563de3e64cf74dce4
rizkyGP <139757784+rizkyGP@users.noreply.github.com>
2025-05-06 03:37:02 +0000

    add COS price and fix filter date

--- erpnext/foms/report/invoice_listing_details/invoice_listing_details.py
+++ erpnext/foms/report/invoice_listing_details/invoice_listing_details.py
@@ -4,6 +4,8 @@
 
 import frappe
 from frappe.utils import flt, cint
+import datetime
+import calendar
 
 def execute(filters=None):
 	return Report(filters).execute()
@@ -18,6 +20,12 @@ class Report():
 			self.cond += " and s.customer = %(customer)s "
 		if self.filters.get("sales_invoice"):
 			self.cond += " and s.name = %(sales_invoice)s "
+
+		today = datetime.date.today()
+		first_day = today.replace(day=1)
+		last_day = today.replace(day=calendar.monthrange(today.year, today.month)[1])
+		self.filters.start_date = self.filters.get("start_date") or first_day
+		self.filters.end_date = self.filters.get("end_date") or last_day
 		self.cond += " and s.posting_date between %(start_date)s and %(end_date)s "
 
 		if not cint(self.filters.get("show_credit_note")):
@@ -32,6 +40,8 @@ class Report():
 			{"fieldname": "item_code", 		"label": "Item", 		"fieldtype": "Link", 	"width":120, "options":"Item"},
 			{"fieldname": "qty", 			"label": "Qty", 		"fieldtype": "Float", 	"width":80, "options":""},
 			{"fieldname": "uom", 			"label": "UOM", 		"fieldtype": "Link", 	"width":180, "options":"UOM"},
+			{"fieldname": "weight", 		"label": "Weight (KG)", "fieldtype": "Float", 	"width":100, "options":""},
+			{"fieldname": "cos", 			"label": "COS", 		"fieldtype": "Currency", "width":100, "options":""},
 			{"fieldname": "rate", 			"label": "Price", 		"fieldtype": "Currency", "width":100, "options":""},
 			{"fieldname": "amount", 		"label": "Amount", 		"fieldtype": "Currency", "width":120, "options":""},
 			{"fieldname": "gst", 			"label": "GST", 		"fieldtype": "Percent", "width":100, "options":""},
@@ -52,12 +62,14 @@ class Report():
 				si.item_tax_rate,
 				si.rate,
 				si.uom,
+				p.total_weight as weight,
 				si.amount,
 				si.net_amount,
 				t.name AS tx_row,
 				t.charge_type,
 				t.rate as gst,
-				t.tax_amount as gst_amount
+				t.tax_amount as gst_amount,
+				sle_out.valuation_rate AS cos
 			FROM
 				`tabSales Invoice` s
 					LEFT JOIN
@@ -66,11 +78,26 @@ class Report():
 				`tabSales Taxes and Charges` t ON t.parent = s.name
 					left join
 				`tabAddress` a on a.name = s.shipping_address_name
+					LEFT JOIN
+				`tabPackaging` p ON p.name = si.uom
+					LEFT JOIN
+				`tabDelivery Note Item` dni ON dni.name = si.dn_detail
+					LEFT JOIN
+				(SELECT 
+					voucher_detail_no, item_code, batch_no, valuation_rate
+				FROM
+					`tabStock Ledger Entry`
+				WHERE
+					voucher_type = 'Delivery Note'
+						AND actual_qty < 0
+						AND is_cancelled = 0) sle_out ON sle_out.voucher_detail_no = dni.name
+					AND sle_out.item_code = dni.item_code
+					AND sle_out.batch_no = dni.batch_no
 			WHERE 
 				s.docstatus = 1
 				{}
 			ORDER BY s.posting_date desc
-		""".format(self.cond), self.filters, as_dict=1, debug=1)
+		""".format(self.cond), self.filters, as_dict=1, debug=0)
 	
 	def process_data(self):
 		self.data = []
