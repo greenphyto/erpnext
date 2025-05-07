878ec0377bbac575e5fbc1c49f667b1de92b2f8f
rizkyGP <139757784+rizkyGP@users.noreply.github.com>
2025-05-07 09:38:02 +0000

    download file and create log

--- erpnext/uob/doctype/uob_sync_log/uob_sync_log.py
+++ erpnext/uob/doctype/uob_sync_log/uob_sync_log.py
@@ -8,17 +8,24 @@ from frappe.utils import now_datetime, cint
 from frappe.model.document import Document
 from erpnext.controllers.uob import UOBAPI
 import traceback
+import base64
+from frappe.utils.file_manager import save_file
 
 class UOBSyncLog(Document):
+	def __init__(self, *args, **kwargs):
+		super(UOBSyncLog, self).__init__(*args, **kwargs)
+		self.uob = UOBAPI()
+
 	def send_file(self, force=False):
 		if not force and self.status == "Complete":
 			return
-		uob = UOBAPI()
 		fn = frappe.get_doc("File", self.file)
 		filepath = fn.get_full_path()
 		filename = fn.file_name
-		res = uob.upload_bank_tx(filepath, filename)
-		print(21, res)
+		res = self.uob.upload_bank_tx(filepath, filename)
+		self.save_result(res)
+
+	def save_result(self, res):
 		self.db_set("status_code", cint(res.get("status_code")))
 		if res.get("status_code") == 200:
 			self.db_set("status", "Complete")
@@ -26,10 +33,34 @@ class UOBSyncLog(Document):
 			self.db_set("status", "Error")
 			error = res.get("result") or res.get("error")
 			self.db_set("error", json.dumps(error))
+	
+	def download_file(self, filename):
+		temp = self.uob.download_bank_tx(fname=filename)
+		self.save_result(temp)
 
-def create_log(csv_data, filename=""):
+		self.db_set("status_code", cint(temp.get("status_code")))
+		if "result" in temp:
+			res = temp['result']
+			encoded_data = res.get("file")
+			binary_data = base64.b64decode(encoded_data)
+			folder="Home/Attachments"
+
+			saved_file = save_file(
+					filename,        # fname (nama file)
+					binary_data,     # file content dalam bytes
+					self.doctype,            # dt = tidak ada dokumen
+					self.name,            # dn = tidak ada nama dokumen
+					folder=folder,
+					is_private=1
+				)
+
+			return saved_file.file_url
+
+
+def create_upload_log(csv_data, filename=""):
 	# create log
 	doc = frappe.new_doc("UOB Sync Log")
+	doc.type = "Upload"
 	doc.insert(ignore_permissions=1)
 
 	try:
@@ -45,6 +76,14 @@ def create_log(csv_data, filename=""):
 		doc.db_set("status", "Error")
 		doc.db_set("error", tb_str)
 		doc.db_set("status_code", 0)
+
+def create_download_log(filename):
+	doc = frappe.new_doc("UOB Sync Log")
+	doc.type = "Download"
+	doc.insert(ignore_permissions=1)
+
+	doc.download_file(filename)
+
 	
 
 def save_csv_to_file(data: list[list[str]], filename: str = None, attach_to_doctype="", attach_to_name="") -> str:
@@ -74,3 +113,14 @@ def save_csv_to_file(data: list[list[str]], filename: str = None, attach_to_doct
 	file_doc.insert(ignore_permissions=True)
 
 	return file_doc  # atau file_doc.name kalau perlu nama dokumen
+
+def get_new_payment_file():
+	uob = UOBAPI()
+	res = uob.get_file_list()
+	for d in res.get("result"):
+		# save
+		create_download_log(d.get("name"))
+	# download
+	# save
+	# read
+	# process
