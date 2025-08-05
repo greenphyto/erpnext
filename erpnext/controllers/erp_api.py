35a2f693049d4a61a4059ec8d9fda9b5e008cea7
rizkyGP <139757784+rizkyGP@users.noreply.github.com>
2025-08-05 03:17:43 +0000

    change response to pure boolean

--- erpnext/controllers/erp_api.py
+++ erpnext/controllers/erp_api.py
@@ -335,7 +335,7 @@ def update_work_order_operation_status(operationNo, percentage=0, rawMaterials=[
 		return {
 			"result": False,
 			"percentage": 0,
-			"message":"Temporary disabled"
+			"error":"Temporary disabled"
 		}
 	
 	data_name = f"Operation {operationNo} Work Order {ERPWorkOrderID}"
@@ -355,7 +355,7 @@ def update_work_order_operation_status(operationNo, percentage=0, rawMaterials=[
 	if temp.get("docstatus") == 1:
 		update_log("Work Order", data_name, "Job Card", temp.get("name"))
 		return {
-			"result": False,
+			"result": True,
 			"percentage": percentage,
 			"message": "Already updated"
 		}
@@ -366,14 +366,15 @@ def update_work_order_operation_status(operationNo, percentage=0, rawMaterials=[
 	if job_card_name and frappe.db.get_value("Stock Entry", {"job_card": job_card_name, "docstatus":1}, cache=False, debug=0):
 		update_log("Work Order", data_name, "Job Card", temp.get("name"))
 		return {
-			"result": False,
+			"result": True,
 			"percentage": percentage,
 			"message": "Already updated (se)"
 		}
 
 	if cint(operationNo) == 3 and not now:
 		return {
-			"result":"Scheduled"
+			"result":True,
+			"message":"Scheduled"
 		}
 	else:
 		return _update_work_order_operation_status(log_res.name, ERPWorkOrderID, operationNo, percentage, rawMaterials)
@@ -396,7 +397,7 @@ def _update_work_order_operation_status(log_name, ERPWorkOrderID, operationNo, p
 	if existing_jc:
 		update_log("Work Order", data_name, "Job Card", existing_jc.name)
 		return {
-			"result": False,
+			"result": True,
 			"percentage": percentage,
 			"message": "Already updated (se)"
 		}
@@ -569,7 +570,8 @@ def submit_work_order_finish_goods(erpWorkOrderID, packets=0, qty=0, expiryDate=
 		)
 	else:
 		return {
-			"result":"Scheduled"
+			"result": True,
+			"message":"Scheduled"
 		}
 
 def _submit_work_order_finish_goods(erpWorkOrderID, packets=0, qty=0, expiryDate="", draft=False, log_name=None):
@@ -593,13 +595,15 @@ def _submit_work_order_finish_goods(erpWorkOrderID, packets=0, qty=0, expiryDate
 	})
 	if not operation_3_status:
 		return {
-			"result":"Operation 3 should be complete first."
+			"result":False,
+			"error":"Operation 3 should be complete first."
 		}
 
 	if status == "Completed":
 		update_log("Work Order", data_name, "Work Order", work_order_name)
 		return {
-			"result":"Already complete"
+			"result":True,
+			"message":"Already complete"
 		}
 	
 	if not work_order_name:
