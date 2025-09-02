6a5403a01d28c893d728df73aa837024144387a8
rizkyGP <139757784+rizkyGP@users.noreply.github.com>
2025-09-02 04:24:31 +0000

    add settings if any email issue

--- erpnext/ai_agent/doctype/ai_agent_settings/ai_agent_settings.js
+++ erpnext/ai_agent/doctype/ai_agent_settings/ai_agent_settings.js
@@ -5,4 +5,34 @@ frappe.ui.form.on('AI Agent Settings', {
 	// refresh: function(frm) {
 
 	// }
+	go_to_er: function(frm){
+		frappe.set_route('Form', 'Scheduled Job Type', 'erp.read_email_inbox');
+	},
+	go_to_es: function(frm){
+		frappe.set_route('Form', 'Scheduled Job Type', 'email_account.resync_email_inbox');
+	},
+	mark_complete_er: function(frm){
+		frappe.call({
+			method:"mark_complete",
+			doc: frm.doc,
+			args:{
+				typ:2
+			},
+			callback: function(){
+				frm.reload_doc()
+			}
+		})
+	},
+	mark_complete_es: function(frm){
+		frappe.call({
+			method:"mark_complete",
+			doc: frm.doc,
+			args:{
+				typ:1
+			},
+			callback: function(){
+				frm.reload_doc()
+			}
+		})
+	}
 });
