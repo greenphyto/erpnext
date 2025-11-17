58ae56825dc4d0a1fde23e2406418973c4f6d153
rizkyGP <139757784+rizkyGP@users.noreply.github.com>
2025-11-17 09:05:50 +0000

    mc; fix issue onload

--- erpnext/selling/doctype/customer/customer.py
+++ erpnext/selling/doctype/customer/customer.py
@@ -55,7 +55,7 @@ class Customer(TransactionBase):
 
 	def set_code(self, force=False):
 		cash_sales = "C00008"
-		comp_abbr = frappe.get_value("Company", self.company, "series_abbr")
+		comp_abbr = cstr(frappe.get_value("Company", self.company, "series_abbr"))
 		series = comp_abbr + "C.#####"
 
 		if self.customer_code:
