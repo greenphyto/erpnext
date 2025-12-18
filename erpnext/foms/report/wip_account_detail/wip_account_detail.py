db42d766e725a6b1b897ccd93d90f9e7e5e2216e
rizkyGP <139757784+rizkyGP@users.noreply.github.com>
2025-12-18 11:22:52 +0000

    add item source price

--- erpnext/foms/report/wip_account_detail/wip_account_detail.py
+++ erpnext/foms/report/wip_account_detail/wip_account_detail.py
@@ -56,6 +56,160 @@ class Report:
             if d.wip_account
         ]
         return accounts
+    
+    def get_item_price_map(self, source="Item Price"):
+        # get all item
+        # get convert to uom
+        # 3 source: Item price, Sales Invoice, Valuation Rate
+        price_map = {}
+        if source == "Item Price":
+            data = frappe.db.sql("""
+                SELECT 
+                    ip.name AS item_price,
+                    ip.price_list,
+                    ip.item_code,
+                    i.item_name,
+                    i.item_group,
+                    ip.currency,
+                    ip.uom AS price_uom,
+                    ip.price_list_rate,
+                    ucd.uom AS conv_uom,
+                    ucd.conversion_factor,
+                    ip.customer,
+                    (1 / ucd.conversion_factor) * ip.price_list_rate as price_conv
+                FROM
+                    `tabItem Price` ip
+                        JOIN
+                    `tabItem` i ON i.item_code = ip.item_code
+                        JOIN
+                    `tabItem Group` ig_products ON ig_products.name = 'Products'
+                        JOIN
+                    `tabItem Group` ig_item ON ig_item.name = i.item_group
+                        AND ig_item.lft BETWEEN ig_products.lft AND ig_products.rgt
+                        LEFT JOIN
+                    `tabUOM Conversion Detail` ucd ON ucd.parenttype = 'Item'
+                        AND ucd.parent = i.name
+                        AND ucd.uom = COALESCE(ip.uom, i.stock_uom)
+                WHERE
+                    ip.selling = 1 AND ip.docstatus < 2
+                ORDER BY ip.item_code , CASE
+                    WHEN COALESCE(ip.uom, i.stock_uom) = i.stock_uom THEN 0
+                    ELSE 1
+                END , CASE
+                    WHEN ip.customer IS NULL OR ip.customer = '' THEN 0
+                    ELSE 1
+                END
+                    """, as_dict=1)
+            for d in data:
+                if d.item_code not in price_map and d.price_conv:
+                    price_map[d.item_code] = d.price_conv
+
+        elif source == "Sales Invoice":
+            data = frappe.db.sql("""
+                SELECT
+                    t.item_code,
+                    t.item_name,
+                    t.sales_invoice,
+                    t.posting_date,
+                    t.uom,
+                    t.qty,
+                    t.rate,
+                    t.conversion_factor,
+                    (t.rate / NULLIF(t.conversion_factor, 0)) AS price_conv
+                FROM (
+                    SELECT
+                        sii.item_code,
+                        sii.item_name,
+                        sii.parent AS sales_invoice,
+                        si.posting_date,
+                        sii.uom,
+                        sii.qty,
+                        sii.rate,
+                        sii.conversion_factor,
+                        ROW_NUMBER() OVER (
+                            PARTITION BY sii.item_code
+                            ORDER BY si.posting_date DESC, si.posting_time DESC, si.creation DESC, sii.creation DESC
+                        ) AS rn
+                    FROM `tabSales Invoice Item` sii
+                    JOIN `tabSales Invoice` si
+                        ON si.name = sii.parent
+                    JOIN `tabItem` i
+                        ON i.item_code = sii.item_code
+                    WHERE
+                        si.docstatus = 1
+                        AND IFNULL(si.is_return, 0) = 0
+                        AND i.stock_uom = 'Kg'
+                ) t
+                WHERE t.rn = 1
+                ORDER BY t.item_code;
+                """, as_dict=1)
+            
+            for d in data:
+                if d.item_code not in price_map and d.price_conv:
+                    price_map[d.item_code] = d.price_conv
+        else:
+            # from Valuation rate
+            data = frappe.db.sql("""
+                SELECT
+                    x.item_code,
+                    x.item_name,
+                    x.warehouse,
+                    x.posting_date,
+                    x.posting_time,
+                    x.voucher_type,
+                    x.voucher_no,
+                    x.actual_qty,
+                    x.incoming_rate as price_conv,
+                    x.valuation_rate
+                FROM (
+                    SELECT
+                        sle.item_code,
+                        i.item_name,
+                        sle.warehouse,
+                        sle.posting_date,
+                        sle.posting_time,
+                        sle.voucher_type,
+                        sle.voucher_no,
+                        sle.actual_qty,
+                        sle.incoming_rate,
+                        sle.valuation_rate,
+                        ROW_NUMBER() OVER (
+                            PARTITION BY sle.item_code
+                            ORDER BY
+                                sle.posting_date DESC,
+                                sle.posting_time DESC,
+                                sle.creation DESC
+                        ) AS rn
+                    FROM
+                        `tabStock Ledger Entry` sle
+                    JOIN
+                        `tabItem` i
+                        ON i.item_code = sle.item_code
+                    JOIN
+                        `tabItem Group` ig_products
+                        ON ig_products.name = 'Products'
+                    JOIN
+                        `tabItem Group` ig_item
+                        ON ig_item.name = i.item_group
+                        AND ig_item.lft BETWEEN ig_products.lft AND ig_products.rgt   -- include child groups
+                    WHERE
+                        sle.docstatus = 1
+                        AND sle.is_cancelled = 0
+                        AND sle.actual_qty > 0              -- incoming only
+                        AND IFNULL(sle.incoming_rate, 0) != 0
+                ) x
+                WHERE
+                    x.rn = 1
+                ORDER BY
+                    x.item_code
+                """, as_dict=1)
+            
+            for d in data:
+                if d.item_code not in price_map and d.price_conv:
+                    price_map[d.item_code] = d.price_conv
+
+        return price_map
+
 
     def get_data(self):
         wip_accounts = self.get_wip_accounts()
@@ -105,6 +259,7 @@ class Report:
 
         # Normalize currency outputs
         je_data = self.get_journal_entry(wip_accounts)
+        price_map = self.get_item_price_map(self.filters.price_source)
         data = []
         for r in rows:
             key = (r.account, r.work_order)
@@ -114,6 +269,8 @@ class Report:
             if not r.amount:
                 continue
 
+            map_price = flt(price_map.get(r.produced_item))
+
             data.append(
                 {
                     "work_order": r.work_order,
@@ -122,8 +279,8 @@ class Report:
                     "account": r.account,
                     "amount": r.amount,
                     "currency": self.company_currency,
-                    "map_price": r.map_price,
-                    "map_currency": r.map_currency or self.company_currency,
+                    "map_price": map_price,
+                    "map_currency": self.company_currency,
                 }
             )
 
