04bd3b83ec6a070d5251c6523e2a298761638305
rizkyGP <139757784+rizkyGP@users.noreply.github.com>
2025-12-29 08:34:48 +0000

    init report picking list report

---
+++ erpnext/foms/report/picking_list_report/picking_list_report.js
@@ -0,0 +1,28 @@
+frappe.query_reports["Picking List Report"] = {
+    "filters": [
+        {
+            "fieldname": "company",
+            "label": __("Company"),
+            "fieldtype": "Link",
+            "options": "Company",
+            "default": frappe.defaults.get_user_default("Company"),
+            "reqd": 1,
+            "width": 100
+        },
+        {
+            "fieldname": "date",
+            "label": __("Date"),
+            "fieldtype": "Date",
+            "reqd": 1,
+            "default": frappe.datetime.get_today()
+        },
+		{
+            "fieldname": "view_type",
+            "label": __("View Type"),
+            "fieldtype": "Select",
+            "reqd": 1,
+            "default": "All Outlets",
+			"options":"All Outlets\nDelivery Note"
+        }
+    ]
+};
