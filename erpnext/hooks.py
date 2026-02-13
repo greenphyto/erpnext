5aac73f2d9abfd3471c7a3a6238b205c0c538243
rizkyGP <139757784+rizkyGP@users.noreply.github.com>
2026-02-13 13:07:28 +0000

    fix hooks error

--- erpnext/hooks.py
+++ erpnext/hooks.py
@@ -413,10 +413,10 @@ doc_events = {
         "on_cancel":"erpnext.controllers.foms.sync_log",
 	},
 	"Delivery Note": {
-        "on_submit":{
+        "on_submit":[
         	"erpnext.controllers.foms.sync_log",
             "erpnext.gp_erp.doctype.consignment_request.consignment_request.billing_consignment_controller"
-		},
+		],
         "on_cancel":[
             "erpnext.controllers.foms.sync_log",
             "erpnext.gp_erp.doctype.consignment_request.consignment_request.billing_consignment_controller"
