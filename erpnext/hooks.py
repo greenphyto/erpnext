f0c7e32e9134fc12ab329d9317521c307de67b44
rizkyGP <139757784+rizkyGP@users.noreply.github.com>
2026-04-20 12:01:51 +0000

    disable autocreate from GRN

--- erpnext/hooks.py
+++ erpnext/hooks.py
@@ -474,7 +474,7 @@ doc_events = {
     "Scheduled Job Log": {
         "after_insert":"erpnext.ai_agent.doctype.ai_agent_settings.ai_agent_settings.read_log"
 	},
-    ("Purchase Order","Purchase Receipt","Purchase Invoice"):{
+    "Purchase Order":{
         "on_submit":"erpnext.controllers.erp.auto_create_selling_from_internal"
 	},
     "Material Request":{
