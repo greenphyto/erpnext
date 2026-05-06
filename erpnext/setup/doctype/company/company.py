7dcad78e04044714ad5cceb473ff73eb87d563d4
rizkyGP <139757784+rizkyGP@users.noreply.github.com>
2026-05-06 13:30:00 +0000

    dont change company if same

--- erpnext/setup/doctype/company/company.py
+++ erpnext/setup/doctype/company/company.py
@@ -4,6 +4,7 @@
 
 import json
 
+import erpnext
 import frappe
 import frappe.defaults
 from frappe import _
@@ -823,6 +824,9 @@ def create_transaction_deletion_request(company):
 	tdr.submit()
 
 def switch_to_company_admin(company, change_user=True):
+	if erpnext.get_default_company() == company:
+		return frappe.session.user
+	
 	user = frappe.get_value("Company", company, "admin_user") or "Administrator"
 	if change_user and user:
 		frappe.set_user(user)
