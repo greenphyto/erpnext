1e8394075a84565734e4a976504cd5b42cde6828
rizkyGP <139757784+rizkyGP@users.noreply.github.com>
2026-05-26 04:50:38 +0000

    fix mayor issue v1

--- erpnext/stock/get_item_details.py
+++ erpnext/stock/get_item_details.py
@@ -58,7 +58,6 @@ def get_item_details(args, doc=None, for_validate=False, overwrite_warehouse=Tru
 	        "set_warehouse": ""
 	}
 	"""
-
 	args = process_args(args)
 	for_validate = process_string_args(for_validate)
 	overwrite_warehouse = process_string_args(overwrite_warehouse)
