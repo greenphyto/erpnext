1e178764b9c1c90dc742a9342f073b3650b7c8e6
rizkyGP <139757784+rizkyGP@users.noreply.github.com>
2026-06-22 10:30:02 +0000

    increase report column length

--- erpnext/gp_erp/report/budget_variance_greenphyto/budget_variance_greenphyto.py
+++ erpnext/gp_erp/report/budget_variance_greenphyto/budget_variance_greenphyto.py
@@ -216,7 +216,7 @@ def get_report_column(filters, period_list):
 		"label": _(period_word+ " (Actual)"),
 		"fieldtype": "Currency",
 		"options": "currency",
-		"width": 200,
+		"width": 250,
 		"align": "right",
 	})
 
@@ -236,7 +236,7 @@ def get_report_column(filters, period_list):
 		"label": _(period_word+ " (Budget)"),
 		"fieldtype": "Currency",
 		"options": "currency",
-		"width": 200,
+		"width": 250,
 		"align": "right",
 	})
 	
@@ -246,7 +246,7 @@ def get_report_column(filters, period_list):
 		"label": _(period_word+ " (Variance $)"),
 		"fieldtype": "Currency",
 		"options": "currency",
-		"width": 200,
+		"width": 250,
 		"align": "right",
 	})
 	
@@ -255,7 +255,7 @@ def get_report_column(filters, period_list):
 		"fieldname": "variance_percent",
 		"label": _(period_word+ " (Variance %)"),
 		"fieldtype": "Percent",
-		"width": 210,
+		"width": 250,
 		"align": "right",
 	})
 
