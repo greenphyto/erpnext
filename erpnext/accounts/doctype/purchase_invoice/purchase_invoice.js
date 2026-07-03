8fccf96b021b977874fd3054987e94742f4f5e6a
rizkyGP <139757784+rizkyGP@users.noreply.github.com>
2026-07-03 03:58:21 +0000

    set is free item if rate = 0

--- erpnext/accounts/doctype/purchase_invoice/purchase_invoice.js
+++ erpnext/accounts/doctype/purchase_invoice/purchase_invoice.js
@@ -819,6 +819,38 @@ frappe.ui.form.on("Purchase Invoice", {
 frappe.ui.form.on("Purchase Invoice Item", {
 	expense_account: function(frm,cdt,cdn){
 		frm.cscript.set_cost_center(frm, cdt,cdn);
+	},
+
+	rate: function(frm, cdt, cdn) {
+		if (frm._in_set_value) return;
+		let row = locals[cdt][cdn];
+
+		if (flt(row.rate) === 0 && flt(row.price_list_rate) > 0) {
+			frappe.model.set_value(cdt, cdn, "original_rate", row.price_list_rate);
+			frm._in_set_value = true;
+			frappe.model.set_value(cdt, cdn, "is_free_item", 1);
+			frm._in_set_value = false;
+		} else if (flt(row.rate) > 0 && cint(row.is_free_item) === 1) {
+			frm._in_set_value = true;
+			frappe.model.set_value(cdt, cdn, "is_free_item", 0);
+			frm._in_set_value = false;
+		}
+	},
+
+	is_free_item: function(frm, cdt, cdn) {
+		if (frm._in_set_value) return;
+		let row = locals[cdt][cdn];
+
+		if (cint(row.is_free_item)) {
+			frappe.model.set_value(cdt, cdn, "original_rate", row.price_list_rate);
+			frm._in_set_value = true;
+			frappe.model.set_value(cdt, cdn, "rate", 0);
+			frm._in_set_value = false;
+		} else {
+			frm._in_set_value = true;
+			frappe.model.set_value(cdt, cdn, "rate", row.original_rate || row.price_list_rate || 0);
+			frm._in_set_value = false;
+		}
 	}
 })
 
