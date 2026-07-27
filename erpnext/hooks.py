db4e821be5227dd6444f188e810923b4bde0c179
rizkyGP <richkey99@gmail.com>
2026-07-27 09:21:11 +0000

    add greenphyto css

--- erpnext/hooks.py
+++ erpnext/hooks.py
@@ -23,7 +23,7 @@ add_to_apps_screen = [
 develop_version = "15.x.x-develop"
 
 app_include_js = "erpnext.bundle.js"
-app_include_css = "erpnext.bundle.css"
+app_include_css = ["erpnext.bundle.css", "https://erp.greenphyto.com/assets/erpnext/dist/css/gp-sg.css"]
 web_include_js = "erpnext-web.bundle.js"
 web_include_css = "erpnext-web.bundle.css"
 email_css = "email_erpnext.bundle.css"
