b9082a49d8be61556c6612a03f807893ce76e1da
rizkyGP <rizky@greenphyto.com>
2026-09-10 06:43:44 +0000

    show budget row on PL

--- erpnext/accounts/report/financial_statements.py
+++ erpnext/accounts/report/financial_statements.py
@@ -252,12 +252,10 @@ def get_data(
 		recompute_monthly_net(accounts_by_name, gl_entries_by_account, period_list)
 	accumulate_values_into_parents(accounts, accounts_by_name, period_list)
 	out = prepare_data(accounts, balance_must_be, period_list, company_currency)
-	if filter_zero_value:
-		out = filter_out_zero_value_rows(out, parent_children_map)
-
-	# Only show based on the budget list
-	elif accounts_to_show:
+	if accounts_to_show:
 		out = filter_out_accounts_except(out, parent_children_map, accounts_to_show)
+	elif filter_zero_value:
+		out = filter_out_zero_value_rows(out, parent_children_map)
 
 
 	#smooth result
